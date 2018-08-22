daea5de5eefdfcd26533804e0cfed5024992e3cf
Artemiy <rendrom@gmail.com>
2018-08-23 01:29:17 +0800

    update mapboxgl tile loading event

---
+++ packages/mapbox-gl-adapter/src/controls/CompassControl.ts
@@ -0,0 +1,8 @@
+import { NavigationControl } from 'mapbox-gl';
+
+export class CompassControl extends NavigationControl {
+    constructor(options = {}) {
+        options = Object.assign({}, options, {showZoom: false});
+        super(options);
+    }
+}
