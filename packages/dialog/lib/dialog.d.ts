c6bf6e8a4b2e74d591997f021675585e447f521b
Artemiy <rendrom@gmail.com>
2018-10-09 13:31:14 +0800

    rebuild dialog

--- packages/dialog/lib/dialog.d.ts
+++ packages/dialog/lib/dialog.d.ts
@@ -1,13 +1,7 @@
 import { DialogAdapter, DialogAdapterOptions } from '@nextgis/webmap';
 import 'dialog-polyfill/dialog-polyfill.css';
 import './dialog.css';
-export interface DialogOptions {
-    template?: string | Node;
-    openers?: HTMLElement[];
-    parent?: Node;
-    closeBtn?: boolean;
-    closeBtnTemplate?: string | Node;
-}
+export { DialogAdapterOptions };
 export declare class Dialog implements DialogAdapter {
     static dialogs: Dialog[];
     options: DialogAdapterOptions;
