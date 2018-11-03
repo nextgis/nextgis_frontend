d59955c93fa8a7de6ae38172a20dcbc33d418bd9
Artemiy <rendrom@gmail.com>
2018-11-04 02:21:57 +0800

    add leaflet ngw image adapter support

---
+++ packages/ngw-leafet/lib/ngw-image-adapter.d.ts
@@ -0,0 +1,6 @@
+import { LayerAdapter } from '@nextgis/webmap';
+export declare class NgwImageAdapter implements LayerAdapter {
+    name: string;
+    layer: any;
+    addLayer(options?: any): any;
+}
