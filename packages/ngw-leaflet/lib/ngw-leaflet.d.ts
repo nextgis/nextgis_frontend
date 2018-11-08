fb02a069fb8cd1501f2daa5eac42e29e5ee0853b
Artemiy <rendrom@gmail.com>
2018-11-08 11:29:57 +0800

    build

---
+++ packages/ngw-leaflet/lib/ngw-leaflet.d.ts
@@ -0,0 +1,5 @@
+import NgwMap, { MapOptions } from '@nextgis/ngw-map';
+import 'leaflet/dist/leaflet.css';
+export default class NgwLeaflet extends NgwMap {
+    constructor(options: MapOptions);
+}
---
+++ packages/ngw-mapbox/lib/ngw-mapbox.d.ts
@@ -0,0 +1,6 @@
+import NgwMap, { MapOptions, NgwLayerOptions } from '@nextgis/ngw-map';
+import 'mapbox-gl/dist/mapbox-gl.css';
+export default class NgwLeaflet extends NgwMap {
+    constructor(options: MapOptions);
+    addNgwLayer(options: NgwLayerOptions): any;
+}
