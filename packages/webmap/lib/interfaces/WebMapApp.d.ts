eaece8677e6824471cea875f89b5264d517bbf5d
Artemiy <rendrom@gmail.com>
2018-11-09 13:37:05 +0800

    webmapControls options

--- packages/webmap/lib/interfaces/WebMapApp.d.ts
+++ packages/webmap/lib/interfaces/WebMapApp.d.ts
@@ -1,12 +1,17 @@
 import { WebLayerEntry } from '../entities/WebLayerEntry';
 import { MapAdapter } from './MapAdapter';
 import { StarterKit } from './AppSettings';
+import { MapControl } from './MapControl';
 export interface MapOptions {
     target: string | HTMLElement;
     logo?: string;
-    controls?: any[];
+    controls?: Array<string | MapControl>;
+    controlsOptions?: {
+        [controlName: string]: any;
+    };
     minZoom?: number;
     center?: [number, number];
+    bounds?: [number, number, number, number];
     zoom?: number;
 }
 export interface AppOptions {
