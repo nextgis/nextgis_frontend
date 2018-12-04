263790d33d58f7e89baeba89eb8de372ff5fdbea
Artemiy <rendrom@gmail.com>
2018-12-04 22:40:57 +0800

    update demo header

--- packages/demo/src/App.vue
+++ packages/demo/src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <v-app id="inspire">
+
     <v-navigation-drawer
       :clipped="$vuetify.breakpoint.lgAndUp"
       :open="open"
@@ -7,37 +8,35 @@
       fixed
       app
     >
-    <v-treeview v-if="items"
-      :active.sync="active"
-      :items="items"
-      :open="open"
-      activatable
-      open-on-click
-      transition
-      @update:open="onOpen"
-    >
-      <template slot="prepend" slot-scope="{ item, open, leaf }">
-        <v-icon v-if="item.icon">
-          {{item.icon}}
-        </v-icon>
-      </template>
-    </v-treeview>
+      <v-treeview
+        v-if="items"
+        :active.sync="active"
+        :items="items"
+        :open="open"
+        dark
+        style="background:#0070c5"
+        activatable
+        open-on-click
+        transition
+        @update:open="onOpen"
+      >
+        <template slot="prepend" slot-scope="{ item, open, leaf }">
+          <v-icon v-if="item.icon">{{item.icon}}</v-icon>
+        </template>
+      </v-treeview>
     </v-navigation-drawer>
-    <v-toolbar
-      :clipped-left="$vuetify.breakpoint.lgAndUp"
-      color="white"
-      app
-      fixed
-    >
+
+    <v-toolbar :clipped-left="$vuetify.breakpoint.lgAndUp" color="#e5eef7" app fixed>
       <v-toolbar-title class="ml-0 pl-0">
         <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-        <span class="">NextGIS Frontend</span>
+        <logo></logo>
+
       </v-toolbar-title>
+      <span class="title ml-3">Frontend</span>
       <v-spacer></v-spacer>
       <v-btn icon large href="https://github.com/nextgis/nextgisweb_frontend" target="_blank">
         <v-icon large>mdi-github-circle</v-icon>
       </v-btn>
-
     </v-toolbar>
     <v-content>
       <component
@@ -46,16 +45,13 @@
         :text="current.name"
         :description="current.description"
         :html="current.html"
-      >
-      </component>
+      ></component>
     </v-content>
-
-
   </v-app>
 </template>
 
 <script lang="ts">
-export { App as default } from './App';
+export { App as default } from "./App";
 </script>
 
 
@@ -77,5 +73,26 @@ code:before,
 kbd:before {
   content: "" !important;
   letter-spacing: 0 !important;
+}
+
+.header {
+    backface-visibility: hidden;
+    background-color: #e5eef7;
+    border-bottom: 1px solid #d3e3f2;
+    color: rgba(0,0,0,.87);
+    font-size: 14px;
+    font-weight: 400;
+    height: 43px;
+    left: 0;
+    line-height: 43px;
+    min-height: 43px;
+    padding-left: 0;
+    padding-right: 0;
+    position: fixed;
+    right: 0;
+    top: 0;
+    white-space: nowrap;
+    width: 100%;
+    z-index: 40;
 }
 </style>
---
+++ packages/demo/src/components/Logo/Logo.ts
@@ -0,0 +1,4 @@
+import { Vue, Component } from 'vue-property-decorator';
+
+@Component
+export class Logo extends Vue {}
