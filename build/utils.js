2f72e30e95f4b18fd8cd3bc1cf0c74aa9849e908
Artemiy <rendrom@gmail.com>
2018-12-13 17:16:48 +0800

    remove css-loader minify option for version 2

--- build/utils.js
+++ build/utils.js
@@ -11,7 +11,6 @@ exports.cssLoaders = function (options) {
   var cssLoader = {
     loader: 'css-loader',
     options: {
-      minimize: process.env.NODE_ENV === 'production',
       sourceMap: options.sourceMap
     }
   }
