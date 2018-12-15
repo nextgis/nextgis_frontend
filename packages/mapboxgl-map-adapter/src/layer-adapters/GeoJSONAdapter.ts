64c69346bd85d63c8a31ad4df38eba7756ef69e0
Artemiy <rendrom@gmail.com>
2018-12-15 17:49:43 +0800

    mapbox paint from cb

--- packages/mapboxgl-map-adapter/src/layer-adapters/GeoJSONAdapter.ts
+++ packages/mapboxgl-map-adapter/src/layer-adapters/GeoJSONAdapter.ts
@@ -2,13 +2,34 @@ import {
   LayerAdapter,
   GeoJsonAdapterOptions,
   GeoJsonAdapterLayerType,
-  GeoJsonAdapterLayerPaint
+  GeoJsonAdapterLayerPaint,
+  GetPaintCallback
 } from '@nextgis/webmap';
 import { BaseAdapter } from './BaseAdapter';
-import { GeoJsonObject, GeoJsonGeometryTypes, Feature, FeatureCollection, GeometryCollection } from 'geojson';
-import { GeoJSONSourceRaw } from 'mapbox-gl';
+import {
+  GeoJsonObject,
+  GeoJsonGeometryTypes,
+  Feature as F,
+  FeatureCollection,
+  GeometryCollection,
+  GeometryObject,
+  Geometry,
+  GeoJsonProperties
+} from 'geojson';
+// import { GeoJSONSourceRaw } from 'mapbox-gl';
 
 let ID = 1;
+
+interface Feature<G extends GeometryObject | null = Geometry, P = GeoJsonProperties> extends F<G, P> {
+  _paint_id?: number;
+}
+
+const allowedParams: Array<[string, string] | string> = ['color', 'opacity'];
+const allowedByType = {
+  circle: allowedParams.concat(['radius']),
+  line: allowedParams.concat([['weight', 'width']]),
+  fill: allowedParams.concat([])
+};
 
 const typeAlias: { [x: string]: GeoJsonAdapterLayerType } = {
   'Point': 'circle',
@@ -34,6 +55,8 @@ const PAINT = {
 
 export class GeoJsonAdapter extends BaseAdapter implements LayerAdapter {
 
+  private _features: Feature[] = [];
+
   addLayer(options?: GeoJsonAdapterOptions): string {
     this.name = options.id || 'geojson-' + ID++;
     const opt = { ...this.options, ...(options || {}) };
@@ -45,7 +68,10 @@ export class GeoJsonAdapter extends BaseAdapter implements LayerAdapter {
       type = typeAlias[detectedType];
     }
 
-    const data = filterGeometries(options.data, type) as any;
+    const data = this.filterGeometries(options.data, type) as any;
+    this._features.forEach((x, i) => {
+      x._paint_id = i;
+    });
     if (data) {
       this.map.addLayer({
         id: String(this.name),
@@ -57,59 +83,72 @@ export class GeoJsonAdapter extends BaseAdapter implements LayerAdapter {
         layout: {
           visibility: 'none',
         },
-        paint: createPaintForType({ ...PAINT, ...(opt.paint || {}) }, type)
+        paint: this.createPaintForType(opt.paint, type)
       });
       return this.name;
     } else {
       throw new Error('No geometry for given type');
     }
   }
-}
 
-function createPaintForType(paint: GeoJsonAdapterLayerPaint, type: GeoJsonAdapterLayerType) {
-  const mapboxPaint = {};
-  const allowedParams: Array<[string, string] | string> = ['color', 'opacity'];
-  const allowedByType = {
-    circle: allowedParams.concat(['radius']),
-    line: allowedParams.concat([['weight', 'width']]),
-    fill: allowedParams.concat([])
-  };
-  for (const p in paint) {
-    if (paint.hasOwnProperty(p)) {
-      const allowedType = allowedByType[type].find((x) => {
-        if (typeof x === 'string') {
-          return x === p;
-        } else if (Array.isArray(x)) {
-          return x[0] === p;
+  private filterGeometries(data: GeoJsonObject, type: GeoJsonAdapterLayerType): GeoJsonObject | boolean {
+    if (data.type === 'FeatureCollection') {
+      const features = (data as FeatureCollection).features = (data as FeatureCollection).features
+        .filter((f) => geometryFilter(f.geometry.type, type));
+      this._features = features;
+    } else if (data.type === 'Feature') {
+      const allow = geometryFilter((data as Feature).geometry.type, type);
+      if (!allow) {
+        return false;
+      }
+      this._features.push((data as Feature));
+    } else if (data.type === 'GeometryCollection') {
+      (data as GeometryCollection).geometries = (data as GeometryCollection).geometries
+        .filter((g) => geometryFilter(g.type, type));
+    }
+    return data;
+  }
+
+  private createPaintForType(paint: GeoJsonAdapterLayerPaint | GetPaintCallback, type: GeoJsonAdapterLayerType) {
+    if (typeof paint === 'function') {
+      const style = {};
+      this._features.forEach((x) => {
+        const _paint = paint(x);
+        for (const p in _paint) {
+          if (_paint.hasOwnProperty(p)) {
+            x.properties['_paint_' + p] = _paint[p];
+            style[p] = ['get', '_paint_' + p];
+          }
         }
       });
-      if (allowedType) {
-        const paramName = Array.isArray(allowedType) ? allowedType[1] : allowedType;
-        mapboxPaint[type + '-' + paramName] = paint[p];
+      // @ts-ignore
+      const styleFromCb = this.createPaintForType(style, type);
+      return styleFromCb;
+    } else {
+      const mapboxPaint = {};
+      const _paint = { ...PAINT, ...(paint || {}) };
+      for (const p in _paint) {
+        if (_paint.hasOwnProperty(p)) {
+          const allowedType = allowedByType[type].find((x) => {
+            if (typeof x === 'string') {
+              return x === p;
+            } else if (Array.isArray(x)) {
+              return x[0] === p;
+            }
+          });
+          if (allowedType) {
+            const paramName = Array.isArray(allowedType) ? allowedType[1] : allowedType;
+            mapboxPaint[type + '-' + paramName] = _paint[p];
+          }
+        }
       }
+      return mapboxPaint;
     }
   }
-  return mapboxPaint;
 }
 
 function geometryFilter(geometry: GeoJsonGeometryTypes, type: GeoJsonAdapterLayerType): boolean {
   return backAliases[type].indexOf(geometry) !== -1;
-}
-
-function filterGeometries(data: GeoJsonObject, type: GeoJsonAdapterLayerType): GeoJsonObject | boolean {
-  if (data.type === 'FeatureCollection') {
-    (data as FeatureCollection).features = (data as FeatureCollection).features
-      .filter((f) => geometryFilter(f.geometry.type, type));
-  } else if (data.type === 'Feature') {
-    const allow = geometryFilter((data as Feature).geometry.type, type);
-    if (!allow) {
-      return false;
-    }
-  } else if (data.type === 'GeometryCollection') {
-    (data as GeometryCollection).geometries = (data as GeometryCollection).geometries
-      .filter((g) => geometryFilter(g.type, type));
-  }
-  return data;
 }
 
 function detectType(geojson: GeoJsonObject): GeoJsonGeometryTypes {
