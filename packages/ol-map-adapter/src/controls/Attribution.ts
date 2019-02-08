e03186f3346c7baf5b88ef0707085cd934d51147
Artemiy <rendrom@gmail.com>
2019-02-08 14:08:24 +0800

    strict for ol-map-adapter

--- packages/ol-map-adapter/src/controls/Attribution.ts
+++ packages/ol-map-adapter/src/controls/Attribution.ts
@@ -1,11 +1,12 @@
 import Attr from 'ol/control/Attribution';
+import { AttributionControlOptions } from '@nextgis/webmap';
 
 const OPTIONS = {
   collapsible: false
 };
 
 export class Attribution extends Attr {
-  constructor(options) {
+  constructor(options: AttributionControlOptions) {
     super({ ...OPTIONS, ...options });
   }
 }
