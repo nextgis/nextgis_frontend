ce8bd9eb4752ec9bd32c282d4de3506354b34369
Artemiy <rendrom@gmail.com>
2019-02-16 22:59:41 +0800

    add deom class item api component

---
+++ packages/demo/src/components/ApiComponent/ItemKinds/ClassItem.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h3>{{item.kindString}} {{item.name}}</h3>
+
+    <div v-for="p in item.children" :key="p.id">
+
+      <p v-if="p.kindString === 'Property'">
+        {{p.kindString}} {{p.name}}
+      </p>
+      <p v-if="p.kindString === 'Method'">
+        {{p.kindString}} {{p.name}}
+      </p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+export { ClassItem as default } from './ClassItem';
+</script>
+
+<style lang="scss">
+</style>
