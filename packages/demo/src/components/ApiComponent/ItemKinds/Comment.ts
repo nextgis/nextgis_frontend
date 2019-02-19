d94da59d8116ae330de892cc997d3ff536d003e8
Artemiy <rendrom@gmail.com>
2019-02-19 21:43:23 +0800

    demo doc comment component

---
+++ packages/demo/src/components/ApiComponent/ItemKinds/Comment.ts
@@ -0,0 +1,19 @@
+import { Vue, Component, Prop } from 'vue-property-decorator';
+
+@Component
+export class Comment extends Vue {
+  @Prop() text: string;
+
+  get str() {
+    return this._prepareStr(this.text);
+  }
+
+  _prepareStr(text: string): string {
+    // `code` > <span>code</span>
+    text = text.replace(/`(.+)`/g, '<span class="code">$1</span>');
+    // {@link url | name}
+    text = text.replace(/{@link (.+) \| (.+)}/g, '<a href="$1">$2</a>');
+    return text;
+  }
+
+}
