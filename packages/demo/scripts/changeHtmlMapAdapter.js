0ab7ce718115e1860703f4da7faaa49bc46e8374
Artemiy <rendrom@gmail.com>
2019-02-26 15:36:25 +0800

    copy examples from demo to appropriate packages

--- packages/demo/scripts/changeHtmlMapAdapter.js
+++ packages/demo/scripts/changeHtmlMapAdapter.js
@@ -1,8 +1,13 @@
 function changeHtmlMapAdapter(html, adapter, adapters) {
-  const reName = new RegExp(adapters.map((x) => x.name).join('|'), 'g');
-  const reVersion = new RegExp(adapters.map((x) => x.version).join('|'), 'g');
-  html = html.replace(reName, adapter.name);
-  html = html.replace(reVersion, adapter.version);
+  if (Array.isArray(adapter)) {
+    const reName = new RegExp(adapters.map((x) => x.name).join('|'), 'g');
+    const reVersion = new RegExp(adapters.map((x) => x.version).join('|'), 'g');
+    html = html.replace(reName, adapter.name);
+    html = html.replace(reVersion, adapter.version);
+  } else {
+    const reName = new RegExp(adapters.map((x) => x).join('|'), 'g');
+    html = html.replace(reName, adapter);
+  }
 
   return html;
 }
