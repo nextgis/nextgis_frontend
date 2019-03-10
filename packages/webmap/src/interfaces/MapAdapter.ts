48a8c7e1cff260d0ad7f27e88b9a8524bfb7056a
Artemiy <rendrom@gmail.com>
2019-03-10 15:58:06 +0800

    doc

--- packages/webmap/src/interfaces/MapAdapter.ts
+++ packages/webmap/src/interfaces/MapAdapter.ts
@@ -15,19 +15,38 @@ import {
 import { MapOptions } from './WebMapApp';
 import { LatLng, LngLatArray, LngLatBoundsArray, Pixel } from './BaseTypes';
 
+/**
+ * Parameters passed to the arguments of the callback function when clicking on the map
+ */
 export interface MapClickEvent {
+  /**
+   * The geographical point where the mouse event occured.
+   */
   latLng: LatLng;
+  /**
+   * Pixel coordinates of the point where the mouse event occured relative to the map сontainer.
+   */
   pixel: Pixel;
+  /**
+   * Map adapter original click event
+   */
   source?: any;
 }
 
+/**
+ * Parameters that control how the fit to object will be work.
+ */
 export interface FitOptions {
+  /**
+   * The maximum possible zoom to use.
+   */
   maxZoom?: number;
 }
 
 export type ControlPositions = 'top-right' | 'top-left' | 'bottom-right' | 'bottom-left';
 
 /**
+ * Parameters and methods that control the behavior of the map and the layers on it.
  * @typeparam M WEB-GIS framework map interface
  * @typeparam M WEB-GIS framework layer interface
  * @typeparam M WEB-GIS framework control interface
@@ -36,20 +55,47 @@ export interface MapAdapter<M = any, L = any, C = any> {
 
   lonlatProjection?: string;
   displayProjection?: string;
+  /**
+   * Original map object of GIS framework (Leaflet.Map, Openlayers.Map, Mapboxgl.Map or other)
+   */
   map?: M;
+  /**
+   * An instance of the EventEmitter class for listen and emit events
+   */
   emitter: EventEmitter;
+  /**
+   * Named adapters for map layers through a generic interface.
+   */
   layerAdapters: { [name: string]: Type<LayerAdapter<M, L, any>> };
+  /**
+   * Named map controls specific for each framework.
+   */
   controlAdapters: { [name: string]: Type<C> };
 
+  create(options?: MapOptions): void;
+
+  /**
+   * Remove layer from Map
+   * @param layer
+   */
   removeLayer(layer: L): any;
+  /**
+   * Check if the given layer on the map
+   * @param layer
+   */
   isLayerOnTheMap?(layer: L): boolean;
+  /**
+   * Set the transparency of given layer.
+   * @param layer
+   * @param opacity
+   */
   setLayerOpacity(layer: L, opacity: number): void;
   showLayer(layer: L): void;
   hideLayer(layer: L): void;
   setLayerOrder(layer: L, order: number, layers?: { [name: string]: LayerAdapter }): void;
 
   fit(extent: LngLatBoundsArray, options?: FitOptions): void;
-  setRotation?(angle: number): void;
+  // setRotation?(angle: number): void;
   setView?(lngLat: LngLatArray, zoom?: number): void;
 
   getZoom(): number | undefined;
@@ -57,8 +103,6 @@ export interface MapAdapter<M = any, L = any, C = any> {
 
   getCenter(): LngLatArray | undefined;
   setCenter(latLng: LngLatArray): void;
-
-  create(options?: MapOptions): void;
 
   onMapLoad(cb?: () => void): Promise<any>;
 
