bd4843eff976f046fc6dca37861d9d461a7a526b
Artemiy <rendrom@gmail.com>
2019-03-11 18:10:09 +0800

    add strick types to webmap events

--- packages/ngw-map/src/decorators.ts
+++ packages/ngw-map/src/decorators.ts
@@ -1,33 +1,9 @@
-import { NgwMap } from './NgwMap';
+import { onLoad } from '@nextgis/webmap';
+import { NgwMapEvents } from './interfaces';
 
 /**
  * Decorator to run action only after map is created
  */
 export function onMapLoad() {
-  return onLoad('map:created');
-}
-
-export function onLoad(event: string) {
-  return function (target: NgwMap, propertyKey: string, descriptor: PropertyDescriptor) {
-    const originalMethod = descriptor.value;
-
-    descriptor.value = function (this: NgwMap, ...args: any[]) {
-
-      return new Promise((resolve, reject) => {
-        const _resolve = () => {
-          const origin = originalMethod.apply(this, args);
-          origin && origin.then ? origin.then(resolve).catch(reject) : resolve(origin);
-        };
-        const isLoaded = this.getEventStatus(event);
-        if (isLoaded) {
-          _resolve();
-        } else {
-          this.emitter.once(event, () => {
-            _resolve();
-          });
-        }
-      });
-    };
-    return descriptor;
-  };
+  return onLoad<NgwMapEvents>('create');
 }
