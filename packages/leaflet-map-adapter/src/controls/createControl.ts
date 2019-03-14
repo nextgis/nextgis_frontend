357375260cdc78acf3c76b8671729cb781c9fdec
Artemiy <rendrom@gmail.com>
2019-03-14 19:15:24 +0800

    leaflet adapter control margin option

--- packages/leaflet-map-adapter/src/controls/createControl.ts
+++ packages/leaflet-map-adapter/src/controls/createControl.ts
@@ -1,14 +1,20 @@
 import { MapControl, CreateControlOptions } from '@nextgis/webmap';
 import { Control, DomEvent } from 'leaflet';
 
-export function createControl(control: MapControl, options: CreateControlOptions = {}): Control  {
+export function createControl(control: MapControl, options: CreateControlOptions = {}): Control {
   const C = Control.extend({
-    onAdd () {
+    onAdd() {
       const element = document.createElement('div');
       const content: HTMLElement = control.onAdd();
       element.classList.add('leaflet-control');
       if (options.bar) {
         element.classList.add('leaflet-bar');
+      }
+      if (!options.margin && !options.margin) {
+        element.style.marginBottom = '0px';
+        element.style.marginTop = '0px';
+        element.style.marginLeft = '0px';
+        element.style.marginRight = '0px';
       }
       element.appendChild(content);
 
@@ -16,7 +22,7 @@ export function createControl(control: MapControl, options: CreateControlOptions
 
       return element;
     },
-    onRemove () {
+    onRemove() {
       control.onRemove();
     }
   });
