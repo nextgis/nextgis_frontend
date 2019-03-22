02b4c3c93e3d2c25dc5da9cd6edb5e82c0c7a77f
Artemiy <rendrom@gmail.com>
2019-03-22 15:29:23 +0800

    ol add layer data with filter

--- packages/ol-map-adapter/src/layer-adapters/GeoJsonAdapter.ts
+++ packages/ol-map-adapter/src/layer-adapters/GeoJsonAdapter.ts
@@ -34,6 +34,7 @@ export class GeoJsonAdapter implements VectorLayerAdapter<Map, Layer, GeoJsonAda
   private vectorSource = new VectorSource();
   private _features: ol.Feature[] = [];
   private _selectedFeatures: ol.Feature[] = [];
+  private _filterFun?: DataLayerFilter<Feature>;
 
   constructor(public map: Map, public options: GeoJsonAdapterOptions) { }
 
@@ -90,8 +91,12 @@ export class GeoJsonAdapter implements VectorLayerAdapter<Map, Layer, GeoJsonAda
       dataProjection: 'EPSG:4326',
       featureProjection: 'EPSG:3857'
     });
-    this.vectorSource.addFeatures(features);
-    this._features = this.vectorSource.getFeatures();
+    this._features = this._features.concat(features);
+    if (this._filterFun) {
+      this.filter(this._filterFun);
+    } else {
+      this.vectorSource.addFeatures(features);
+    }
   }
 
   select(findFeatureFun?: (opt: { feature: Feature }) => boolean) {
@@ -128,16 +133,21 @@ export class GeoJsonAdapter implements VectorLayerAdapter<Map, Layer, GeoJsonAda
     });
   }
 
-  filter(fun: DataLayerFilter<Feature, Layer>) {
+  filter(fun?: DataLayerFilter<Feature, Layer>) {
+    this._filterFun = fun;
     const features = this._features;
-    const filtered = features.filter((feature) => {
-      return fun({feature: getFeature(feature)});
-    });
+    const filtered = fun ? features.filter((feature) => {
+      return fun({ feature: getFeature(feature) });
+    }) : features;
     this.vectorSource.clear();
     const length = filtered.length;
     for (let fry = 0; fry < length; fry++) {
       this.vectorSource.addFeature(filtered[fry]);
     }
+  }
+
+  cleanFilter() {
+    this.filter();
   }
 
   private setPaintEachLayer(paint: GetPaintCallback | GeoJsonAdapterLayerPaint) {
