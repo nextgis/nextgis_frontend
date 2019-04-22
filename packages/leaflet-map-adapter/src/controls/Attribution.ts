279fb3d0bde6c7670d0b9c025250fb6062ca5e0c
Artemiy <rendrom@gmail.com>
2019-04-22 16:47:04 +0800

    update leflet attribution plugin

--- packages/leaflet-map-adapter/src/controls/Attribution.ts
+++ packages/leaflet-map-adapter/src/controls/Attribution.ts
@@ -6,14 +6,15 @@ export class AttributionControl extends Control.Attribution {
   // options: AttributionControlOptions
   constructor(options: AttributionControlOptions & ControlOptions) {
     super(options);
-    const customAttribution = options.customAttribution;
-    let attributions: string[] = [];
-    if (Array.isArray(customAttribution)) {
-      attributions = customAttribution;
-    } else if (customAttribution) {
-      attributions = [customAttribution];
+    const customAttribution = options && options.customAttribution;
+    if (customAttribution) {
+      let attributions: string[] = [];
+      if (Array.isArray(customAttribution)) {
+        attributions = customAttribution;
+      } else if (customAttribution) {
+        attributions = [customAttribution];
+      }
+      attributions.forEach((x) => this.addAttribution(x));
     }
-    attributions.forEach((x) => this.addAttribution(x));
   }
-
 }
