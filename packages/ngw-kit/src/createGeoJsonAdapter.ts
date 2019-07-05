a2556e2fe84a7b943c13ab4bf4edf74772b71ec7
Artemiy <rendrom@gmail.com>
2019-07-06 00:48:36 +0800

    Update interface for Ngw resource adapter

--- packages/ngw-kit/src/createGeoJsonAdapter.ts
+++ packages/ngw-kit/src/createGeoJsonAdapter.ts
@@ -23,18 +23,21 @@ export async function createGeoJsonAdapter(
     // TODO: rewrite all to feature api after NGW cloud update
     // need geojson and 4326 params
     if (filters) {
-      const filterOptions: { [filter: string]: any } = {};
+      const url = '/api/resource/{id}/feature/';
+      const params: string[] = [];
       filters.forEach(([field, operation, value]) => {
-        filterOptions[`fld_${field} __${operation}`] = value;
+        params.push(`fld_${field}__${operation}=${value}`);
       });
-      return connector.makeQuery('api/resource/{id}/feature', {
+      return connector.makeQuery(url + '?' + params.join('&') , {
         id: options.resourceId,
-        ...filterOptions
       }).then((x: FeatureItem[]) => {
         const features: Array<Feature<Point>> = x.map((y) => {
-          const f = NgwKit.utils.wktToGeoJson(y.geom);
-          f.properties = y.fields;
-          return f;
+          const geometry = NgwKit.utils.wktToGeoJson(y.geom);
+          return {
+            type: 'Feature',
+            properties: y.fields,
+            geometry
+          };
         });
 
         const geojson: FeatureCollection<Point> = {
@@ -48,13 +51,12 @@ export async function createGeoJsonAdapter(
         id: options.resourceId
       }).then((resp) => {
         _fullDataLoad = true;
-        return resp;
+        return toWgs84(resp);
       });
     }
   };
 
   const onLoad = (data: GeoJsonObject) => {
-    data = toWgs84(data);
     const geoJsonOptions: GeoJsonAdapterOptions = {
       data,
     };
