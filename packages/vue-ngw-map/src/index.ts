fefe46fff5b94ecc6b4a368aa028721c1cc37243
Artemiy <rendrom@gmail.com>
2019-09-23 16:21:30 +0800

    Craete vuex-ngw package

--- packages/vue-ngw-map/src/index.ts
+++ packages/vue-ngw-map/src/index.ts
@@ -2,9 +2,8 @@
  * @module vue-ngw-map
  */
 import { VueNgwMap } from './components/VueNgwMap';
-import { ResourceStore, createResourceStore } from './store/ResourceStore/ResourceStore';
 
 export * from './components/VueNgwResource';
 
-export { VueNgwMap, createResourceStore, ResourceStore };
+export { VueNgwMap };
 export default VueNgwMap;
