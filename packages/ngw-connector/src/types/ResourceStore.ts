a3e52df4662e04eec85e0b7d218bebcf482e7374
Artemiy <rendrom@gmail.com>
2019-09-24 14:57:06 +0800

    Update NgwConnector Store interface

--- packages/ngw-connector/src/types/ResourceStore.ts
+++ packages/ngw-connector/src/types/ResourceStore.ts
@@ -2,7 +2,9 @@
  * @module ngw-connector
  */
 
-export interface ResourceStoreItem {
+export type ResourceStoreItem<P extends Record<string, any>> = ResourceStoreItemDefault & P;
+
+export interface ResourceStoreItemDefault {
   id: number;
   label: string;
   [field: string]: string | number | boolean | null | Date;
