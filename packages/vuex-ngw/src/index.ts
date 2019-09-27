c96fd3d14e307c3099a255d3adbdf5f136876d2a
Artemiy <rendrom@gmail.com>
2019-09-27 14:30:58 +0800

    Update vues resource store

--- packages/vuex-ngw/src/index.ts
+++ packages/vuex-ngw/src/index.ts
@@ -4,4 +4,5 @@
 import { ResourceStore, createResourceStore } from './store/ResourceStore/ResourceStore';
 
 export { createResourceStore, ResourceStore };
+export * from './interfaces';
 export default ResourceStore;
