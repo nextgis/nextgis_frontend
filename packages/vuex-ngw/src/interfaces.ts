85b1e1e09b74ff0186fec88b994d687eaaf8ea24
Artemiy <rendrom@gmail.com>
2019-10-04 14:42:11 +0800

    Clean interface

--- packages/vuex-ngw/src/interfaces.ts
+++ packages/vuex-ngw/src/interfaces.ts
@@ -1,31 +1,4 @@
-import { Polygon as LPolygon } from 'leaflet';
-import { VectorResourceAdapter } from '@nextgis/ngw-kit';
-import { LayerDefinition } from '@nextgis/webmap';
-import { Feature, Polygon, Point, Geometry, GeoJsonProperties } from 'geojson';
-
-export type PlotFeature = Feature<Polygon, PlotProperties>;
-export type PlotLayerDefinition = LayerDefinition<PlotFeature>;
-export type PlotResourceAdapter = VectorResourceAdapter<any, LPolygon, any, PlotFeature>;
-
-export interface PlotProperties {
-  subj: string;
-  rayon: string;
-}
-
-export type TurnPointType = 0 | 1 | 2;
-
-export interface ReferencePointProperty extends TurnPointProperties {
-  type: 0;
-  idpnt: 0;
-}
-
-export type ReferencePointFeature = Feature<Point, ReferencePointProperty>;
-
-export interface TurnPointProperties {
-  idpnt: number;
-  type: TurnPointType;
-  plotid: number;
-}
+import { Feature, Geometry, GeoJsonProperties } from 'geojson';
 
 export type LookupTables = { [field: string]: Record<string, string> };
 
