9d872a528eff63ae195d2478396c99249147c216
Artemiy <rendrom@gmail.com>
2019-11-01 00:30:13 +0800

    Clean

--- jest.config.js
+++ jest.config.js
@@ -1,34 +1,13 @@
-// const base = require("./jest.config.base.js");
-
-// module.exports = {
-//   ...base,
-//   projects:
-//     [
-//       "<rootDir>/packages/*/jest.config.js"
-//     ],
-//   coverageDirectory: "<rootDir>/coverage/"
-// };
-
-
 module.exports = {
-  collectCoverageFrom: [
-    '**/packages/*/**/*.js',
-    '**/packages/*/**/*.ts',
-    '!**/__tests__/**', ,
-  ],
-  modulePathIgnorePatterns: [
-
-  ],
+  collectCoverageFrom: ['**/packages/*/**/*.js', '**/packages/*/**/*.ts', '!**/__tests__/**'],
+  modulePathIgnorePatterns: [],
   projects: ['<rootDir>'],
-  testPathIgnorePatterns: [
-    '/node_modules/',
-    '\\.snap$'
-  ],
+  testPathIgnorePatterns: ['/node_modules/', '\\.snap$'],
   transform: {
     '^.+\\.tsx?$': 'ts-jest'
   },
   watchPlugins: [
     // 'jest-watch-typeahead/filename',
     // 'jest-watch-typeahead/testname',
-  ],
+  ]
 };
