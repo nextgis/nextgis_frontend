5f46a2abf9fbf586adee16374a6c1c0397c546f4
Artemiy <rendrom@gmail.com>
2019-11-06 13:06:17 +0800

    Reusable tests for MapAdapter begin

--- packages/webmap/__tests__/webmap.spec.ts
+++ packages/webmap/__tests__/webmap.spec.ts
@@ -1,44 +1,4 @@
-const fs = require('fs');
-const path = require('path');
-const html = fs.readFileSync(path.resolve(__dirname, '../../../tests/index.html'), 'utf8');
+import { mapAdapterTests } from '../../../tests/mapAdapterTests';
+import { MapAdapter } from './classes/MapAdapter';
 
-import { WebMap, AppOptions } from '../src';
-
-import { LeafletMapAdapter as MapAdapter } from '../../leaflet-map-adapter/src/LeafletMapAdapter';
-
-jest.dontMock('fs');
-
-const adapterName = 'LeafletMapAdapter';
-const createWebMap = (options?: AppOptions) => {
-  return new WebMap({ mapAdapter: new MapAdapter(), ...options });
-};
-
-describe('Initialize WebMap', () => {
-  beforeEach(() => {
-    document.documentElement.innerHTML = html.toString();
-  });
-
-  afterEach(() => {
-    // restore the original func after test
-    jest.resetModules();
-  });
-
-  it(`Apply adapter ${adapterName}`, () => {
-    const mapAdapter = new MapAdapter();
-    const webMap = new WebMap({ mapAdapter });
-    expect(webMap.mapAdapter).toBe(mapAdapter);
-  });
-
-  it('Manual create', () => {
-    const webMap = createWebMap();
-    const spy = new Promise(resolve => {
-      webMap.emitter.on('create', () => {
-        resolve();
-      });
-    });
-    webMap.create();
-    spy.then(() => {
-      expect(document.getElementById('map1')).toBeTruthy();
-    });
-  });
-});
+mapAdapterTests(MapAdapter);
