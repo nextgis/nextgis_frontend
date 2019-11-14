994945c6009318a61414ace0b1de96a79b360608
Artemiy <rendrom@gmail.com>
2019-11-14 11:01:41 +0800

    Create test script without coverage

--- test/webpack.config.js
+++ test/webpack.config.js
@@ -1,8 +1,3 @@
-// const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-// let HtmlWebpackPlugin = require('html-webpack-plugin');
-// let FaviconsWebpackPlugin = require('favicons-webpack-plugin')
-const path = require('path');
-
 let alias = {};
 try {
   const { getAliases } = require('../build/aliases');
@@ -11,50 +6,46 @@ try {
   // ignore
 }
 
-// const include = [
-//   path.resolve('packages/webmap/src/'),
-//   path.resolve('packages/utils/src/'),
-//   path.resolve('test/specs/')
-// ];
-
-module.exports = {
-  mode: 'development',
-
-  devtool: 'inline-source-map',
-
-  resolve: {
-    extensions: ['.js', '.ts', '.json'],
-    alias
-  },
-
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        use: [
-          {
-            loader: 'ts-loader',
-            options: {
-              transpileOnly: true
-            }
+module.exports = (opt = { coverage: false }) => {
+  const rules = [
+    {
+      test: /\.ts$/,
+      use: [
+        {
+          loader: 'ts-loader',
+          options: {
+            transpileOnly: true
           }
-        ],
-        exclude: [/node_modules/]
-        // include
-      },
-      {
-        test: /\.ts$/,
-        exclude: [/node_modules/],
-        enforce: 'post',
-        use: {
-          loader: 'istanbul-instrumenter-loader',
-          options: { esModules: true }
         }
-      },
-      {
-        test: /\.css$/,
-        use: [{ loader: 'css-loader', options: { sourceMap: true } }]
+      ],
+      exclude: [/node_modules/]
+      // include
+    },
+    {
+      test: /\.css$/,
+      use: [{ loader: 'css-loader', options: { sourceMap: true } }]
+    }
+  ];
+  if (opt.coverage) {
+    rules.push({
+      test: /\.ts$/,
+      exclude: [/node_modules/],
+      enforce: 'post',
+      use: {
+        loader: 'istanbul-instrumenter-loader',
+        options: { esModules: true }
       }
-    ]
+    });
   }
+  return {
+    mode: 'development',
+    devtool: 'inline-source-map',
+    resolve: {
+      extensions: ['.js', '.ts', '.json'],
+      alias
+    },
+    module: {
+      rules
+    }
+  };
 };
