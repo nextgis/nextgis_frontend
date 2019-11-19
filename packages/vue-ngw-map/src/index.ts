718887014b64f6fcc4186c805ffa719e98e44d72
Artemiy <rendrom@gmail.com>
2019-11-19 15:34:52 +0800

    Add VueNgwControl component

--- packages/vue-ngw-map/src/index.ts
+++ packages/vue-ngw-map/src/index.ts
@@ -4,6 +4,7 @@
 import { VueNgwMap } from './components/VueNgwMap';
 
 export * from './components/VueNgwResource';
+export * from './components/VueNgwControl';
 
 export { VueNgwMap };
 export default VueNgwMap;
