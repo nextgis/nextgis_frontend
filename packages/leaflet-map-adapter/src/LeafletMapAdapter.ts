98e1d29df200ced6b7261feb7e0d5f84347c883f
Artemiy <rendrom@gmail.com>
2019-11-23 20:19:39 +0800

    Eslint fix

--- packages/leaflet-map-adapter/src/LeafletMapAdapter.ts
+++ packages/leaflet-map-adapter/src/LeafletMapAdapter.ts
@@ -146,10 +146,7 @@ export class LeafletMapAdapter implements MapAdapter<Map, any, Control> {
   fitBounds(e: LngLatBoundsArray) {
     if (this.map) {
       // top, left, bottom, right
-      this.map.fitBounds([
-        [e[3], e[0]],
-        [e[1], e[2]]
-      ]);
+      this.map.fitBounds([[e[3], e[0]], [e[1], e[2]]]);
     }
   }
 
