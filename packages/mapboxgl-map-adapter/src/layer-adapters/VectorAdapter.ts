904f296305df601d448c18b1c45a92613e532b79
Artemiy <rendrom@gmail.com>
2019-12-05 16:23:49 +0800

    Fix mapbox vector icon style

--- packages/mapboxgl-map-adapter/src/layer-adapters/VectorAdapter.ts
+++ packages/mapboxgl-map-adapter/src/layer-adapters/VectorAdapter.ts
@@ -34,7 +34,7 @@ import { typeAliasForFilter, allowedByType } from '../util/geom_type';
 export interface Feature<
   G extends GeometryObject | null = Geometry,
   P = GeoJsonProperties
-> extends F<G, P> {
+  > extends F<G, P> {
   _rendromId?: string;
 }
 
@@ -48,7 +48,7 @@ type MapboxLayerType = 'fill' | 'line' | 'symbol' | 'circle';
 
 export abstract class VectorAdapter<
   O extends VectorAdapterOptions = VectorAdapterOptions
-> extends BaseAdapter<O>
+  > extends BaseAdapter<O>
   implements VectorLayerAdapter<Map, TLayer, O, Feature> {
   selected = false;
 
@@ -143,7 +143,7 @@ export abstract class VectorAdapter<
               ? paint
               : this.options.nativePaint;
         } else {
-          _paint = await this._createPaintForType(paint, type);
+          _paint = await this._createPaintForType(paint, type, name);
         }
 
         if ('icon-image' in _paint) {
