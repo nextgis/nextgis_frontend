38001390f97fb9317d6592b2700b02aa4c4a2a25
Artemiy <rendrom@gmail.com>
2019-12-31 11:30:29 +0800

    New vue-ngw-ol library added

---
+++ packages/vue-ngw-ol/src/components/VueNgwOl.ts
@@ -0,0 +1,12 @@
+import 'ol/ol.css';
+import Map from 'ol/Map';
+import { Mixins, Prop } from 'vue-property-decorator';
+import Component from 'vue-class-component';
+import MapAdapter from '@nextgis/ol-map-adapter';
+import { VueNgwMap } from '@nextgis/vue-ngw-map';
+
+@Component
+export class VueNgwOl extends Mixins<VueNgwMap<Map>>(VueNgwMap) {
+  @Prop({ type: Object, default: () => new MapAdapter() })
+  readonly mapAdapter!: MapAdapter;
+}
