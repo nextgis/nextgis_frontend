b2401594acf2176996834cbcf454c724c56a70a2
Artemiy <rendrom@gmail.com>
2019-12-31 11:20:08 +0800

    Vue map libraries splitted by adapter

--- packages/vue-ngw-map/src/components/VueNgwMap.ts
+++ packages/vue-ngw-map/src/components/VueNgwMap.ts
@@ -1,21 +1,59 @@
-import L, { Map } from 'leaflet';
-import { Mixins, Prop } from 'vue-property-decorator';
+import Vue, { VNode, VNodeData, CreateElement } from 'vue';
+import { Prop } from 'vue-property-decorator';
 import Component from 'vue-class-component';
-import MapAdapter from '@nextgis/leaflet-map-adapter';
-import { VueNgwMapBase } from './VueNgwMapBase';
+import NgwMap, { NgwMapOptions, MapAdapter } from '@nextgis/ngw-map';
 
-import 'leaflet/dist/leaflet.css';
-// @ts-ignore
-delete L.Icon.Default.prototype._getIconUrl;
-
-L.Icon.Default.mergeOptions({
-  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
-  iconUrl: require('leaflet/dist/images/marker-icon.png'),
-  shadowUrl: require('leaflet/dist/images/marker-shadow.png')
-});
+import NgwConnector from '@nextgis/ngw-connector';
 
 @Component
-export class VueNgwMap extends Mixins<VueNgwMapBase<Map>>(VueNgwMapBase) {
-  @Prop({ type: Object, default: () => new MapAdapter() })
-  mapAdapter!: MapAdapter;
+export class VueNgwMap<M = any> extends Vue {
+  @Prop({ type: Object }) mapAdapter!: MapAdapter;
+  @Prop({ type: Boolean }) fullFilling!: boolean;
+  @Prop({ type: NgwConnector }) connector!: NgwConnector;
+  @Prop({ type: String }) baseUrl!: string;
+  @Prop({ type: Number }) qmsId!: string;
+  @Prop({ type: String }) webMapId!: string;
+  @Prop({ type: Object }) mapOptions!: NgwMapOptions;
+  name = 'vue-ngw-map';
+  ngwMap!: NgwMap<M>;
+  ready = false;
+
+  mounted() {
+    this._setNgwMap();
+  }
+
+  beforeDestroy() {
+    this.ngwMap.destroy();
+  }
+
+  render(h: CreateElement): VNode {
+    const staticStyle: { [param: string]: string } = {
+      zIndex: '0'
+    };
+    if (this.fullFilling) {
+      staticStyle.width = '100%';
+      staticStyle.height = '100%';
+    }
+
+    const data: VNodeData = {
+      staticClass: 'vue-ngw-map',
+      staticStyle,
+      // 'class': this.classes,
+      attrs: { 'data-app': true }
+      // domProps: { id: this.id }
+    };
+
+    return this.ready ? h('div', data, this.$slots.default) : h('div', data);
+  }
+
+  private _setNgwMap() {
+    this.ngwMap = new NgwMap(this.mapAdapter, {
+      ...this.$props,
+      ...this.mapOptions,
+      target: this.$el as HTMLElement
+    });
+    this.ready = true;
+  }
 }
+
+export default VueNgwMap;
--- packages/vue-ngw-mapbox/src/components/VueNgwMapbox.ts
+++ packages/vue-ngw-mapbox/src/components/VueNgwMapbox.ts
@@ -3,12 +3,12 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 import { Map } from 'mapbox-gl';
 import { Mixins, Prop } from 'vue-property-decorator';
 import Component from 'vue-class-component';
-import { VueNgwMapBase } from './VueNgwMapBase';
+import { VueNgwMap } from '@nextgis/vue-ngw-map';
 
 import MapAdapter from '@nextgis/mapboxgl-map-adapter';
 
 @Component
-export class VueNgwMapbox extends Mixins<VueNgwMapBase<Map>>(VueNgwMapBase) {
+export class VueNgwMapbox extends Mixins<VueNgwMap<Map>>(VueNgwMap) {
   @Prop({ type: Object, default: () => new MapAdapter() })
   mapAdapter!: MapAdapter;
 }
