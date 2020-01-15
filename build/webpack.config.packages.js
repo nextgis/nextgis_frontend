bdf6a29b4f2024c5424b1c9f2390afb8de486aff
Artemiy <rendrom@gmail.com>
2020-01-15 17:08:53 +0800

    build: add new `build-tools` library

--- build/webpack.config.packages.js
+++ build/webpack.config.packages.js
@@ -24,7 +24,9 @@ let builded = [];
 
 async function createPromise(forBuild) {
   const modules = forBuild.map(x => x.name);
-  const compileObjs = forBuild.map(x => require(x.webpackPath)(null, { mode: 'production' })[0]);
+  const compileObjs = forBuild.map(
+    x => require(x.webpackPath)(null, { mode: 'production' })[0]
+  );
   const names = modules.map(x => x.replace('@nextgis/', '')).join(', ');
   process.stdout.write('start build ' + names + '\n');
   forBuild.forEach(async x => {
@@ -60,7 +62,9 @@ function generate(source = './packages') {
           const webpackPath = join(libPath, 'webpack.config.js');
           if (existsSync(webpackPath)) {
             const deps = package.dependencies
-              ? Object.keys(package.dependencies).filter(x => x.indexOf('@nextgis') !== -1)
+              ? Object.keys(package.dependencies).filter(
+                  x => x.indexOf('@nextgis') !== -1
+                )
               : [];
             modules[package.name] = {
               path: libPath,
@@ -83,7 +87,8 @@ async function createOrderedBuild() {
       if (builded.indexOf(s) === -1) {
         const { node } = sorted.get(s);
         const canBuild =
-          node.deps.length === 0 || node.deps.filter(d => builded.indexOf(d) === -1).length === 0;
+          node.deps.length === 0 ||
+          node.deps.filter(d => builded.indexOf(d) === -1).length === 0;
         if (canBuild) {
           forBuild.push(node);
         }
