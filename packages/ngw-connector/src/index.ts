a687a8fe0b8389adeb2a6d6f97db330c2f60ad48
Artemiy <rendrom@gmail.com>
2020-02-07 20:56:08 +0800

    wip(util): move CancelablePromise to util
    BREAKING CHANGE: CancelablePromise has been removed from ngw-connecter. Now you should do this: import {CancelablePromise} from '@nextgis/utils;'

--- packages/ngw-connector/src/index.ts
+++ packages/ngw-connector/src/index.ts
@@ -4,13 +4,16 @@
 
 import { NgwConnector } from './NgwConnector';
 
-export { CancelablePromise } from './CancelablePromise';
-
 export * from './interfaces';
 export * from './types/ResourceItem';
 export * from './types/FeatureLayer';
 export * from './types/RequestItemsParamsMap';
 export * from './types/ResourceStore';
+
+// for backward compatibility
+// TODO: safe remove backward compatibility
+import { CancelablePromise } from '@nextgis/utils';
+export { CancelablePromise };
 
 export { NgwConnector };
 export default NgwConnector;
