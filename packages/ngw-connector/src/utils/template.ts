41f81cc9e75ef9fd5534195b2d5f11fd714b36f7
Artemiy <rendrom@gmail.com>
2020-02-12 17:19:43 +0800

    wip(ngw-connector): make connector for browser and node.js

---
+++ packages/ngw-connector/src/utils/template.ts
@@ -0,0 +1,15 @@
+// https://github.com/Leaflet/Leaflet/blob/b507e21c510b53cd704fb8d3f89bb46ea925c8eb/src/core/Util.js#L165
+const templateRe = /\{ *([\w_-]+) *\}/g;
+
+export function template(str: string, data: { [param: string]: any }) {
+  return str.replace(templateRe, (s, key) => {
+    let value = data[key];
+
+    if (value === undefined) {
+      throw new Error('No value provided for letiable ' + s);
+    } else if (typeof value === 'function') {
+      value = value(data);
+    }
+    return value;
+  });
+}
