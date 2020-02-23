dc11a31c234d2ce273187b66eddf3fe54dc6c4a0
Artemiy <rendrom@gmail.com>
2020-02-23 22:21:04 +0800

    build(cesium): copy static

--- packages/ngw-cesium/webpack.config.js
+++ packages/ngw-cesium/webpack.config.js
@@ -1,11 +1,12 @@
 const path = require('path');
 const package = require('./package.json');
 const common = require('../../build/webpack.config');
+const CopywebpackPlugin = require('copy-webpack-plugin');
 
 const library = 'NgwCesium';
 
-// const cesiumSource = '../../node_modules/cesium/Source';
-// const cesiumWorkers = '../Build/Cesium/Workers';
+const cesiumSource = '../../node_modules/cesium/Source';
+const cesiumWorkers = '../Build/Cesium/Workers';
 
 module.exports = (env, argv) => {
   const config = common(env, argv, {
@@ -18,5 +19,21 @@ module.exports = (env, argv) => {
   config.node = { fs: 'empty' };
   config.amd = { toUrlUndefined: true };
   // config.resolve.alias.cesium = path.resolve(__dirname, cesiumSource);
+  config.plugins = config.plugins || [];
+  config.plugins.push(
+    new CopywebpackPlugin([
+      { from: path.join(cesiumSource, cesiumWorkers), to: 'Workers' }
+    ])
+  );
+  config.plugins.push(
+    new CopywebpackPlugin([
+      { from: path.join(cesiumSource, 'Assets'), to: 'Assets' }
+    ])
+  );
+  config.plugins.push(
+    new CopywebpackPlugin([
+      { from: path.join(cesiumSource, 'Widgets'), to: 'Widgets' }
+    ])
+  );
   return config;
 };
