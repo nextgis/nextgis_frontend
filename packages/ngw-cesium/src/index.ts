b824fbb4905eb98a9d2883af10c2743f73370faf
Artemiy <rendrom@gmail.com>
2020-02-24 22:59:56 +0800

    docs: changelog

--- packages/ngw-cesium/src/index.ts
+++ packages/ngw-cesium/src/index.ts
@@ -19,7 +19,9 @@ export class NgwCesium extends NgwMap {
 export default NgwCesium;
 
 // (window as any).CESIUM_BASE_URL = window.location.href;
-(window as any).CESIUM_BASE_URL = (window as any).CESIUM_BASE_URL || 'https://cesium.com/downloads/cesiumjs/releases/1.66/Build/Cesium';
+(window as any).CESIUM_BASE_URL =
+  (window as any).CESIUM_BASE_URL ||
+  'https://cesium.com/downloads/cesiumjs/releases/1.66/Build/Cesium';
 // @ts-ignore
 if (window && !window.NgwMap) {
   // @ts-ignore
