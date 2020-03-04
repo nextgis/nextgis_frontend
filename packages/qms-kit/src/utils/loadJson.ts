f8035e2f2cc63a471cfa5b57a9c841e8beabadad
Artemiy <rendrom@gmail.com>
2020-03-04 14:23:33 +0800

    fix(ngw-map): not block when error on add qms layer

---
+++ packages/qms-kit/src/utils/loadJson.ts
@@ -0,0 +1,23 @@
+/**
+ * @module qms-kit
+ */
+import { fixUrlStr } from '@nextgis/utils';
+
+export function loadJson<T = any>(url: string): Promise<T> {
+  return new Promise<T>((resolve, reject) => {
+    const xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = () => {
+      if (xmlHttp.readyState === 4 && xmlHttp.status === 200) {
+        if (xmlHttp.responseText) {
+          try {
+            resolve(JSON.parse(xmlHttp.responseText));
+          } catch (er) {
+            reject(er);
+          }
+        }
+      }
+    };
+    xmlHttp.open('GET', fixUrlStr(url), true); // true for asynchronous
+    xmlHttp.send();
+  });
+}
