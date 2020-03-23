5fda2a4fb0245fd340bccb3cea466de38a5fb747
Artemiy <rendrom@gmail.com>
2020-03-23 10:18:31 +0800

    build: fix aliases export

--- build/aliases.js
+++ build/aliases.js
@@ -1,4 +1,4 @@
-const { getAliases } = require('../packages/build-tools/aliases');
+const { getAliases } = require('../packages/build-tools');
 
 // backward compatibility
 exports.getAliases = getAliases;
