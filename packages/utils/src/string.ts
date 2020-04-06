68d7a0593a2482c47d8a181c9e7c379b4c2e78f2
Artemiy <rendrom@gmail.com>
2020-04-06 14:18:49 +0800

    feat(utils): update string util

--- packages/utils/src/string.ts
+++ packages/utils/src/string.ts
@@ -1,1 +1,4 @@
-export const capitalize = (str: string) => str[0].toUpperCase() + str.slice(1);
+export const capitalize = (str: string) => {
+  str = String(str).toLowerCase();
+  return str[0].toUpperCase() + str.slice(1);
+};
