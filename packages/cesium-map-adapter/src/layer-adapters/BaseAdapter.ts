07bacb7c82dfc82fd87883708b80b4a6a3443a53
Artemiy <rendrom@gmail.com>
2020-05-10 17:42:08 +0800

    feat(cesium): get extent of tileset3D

--- packages/cesium-map-adapter/src/layer-adapters/BaseAdapter.ts
+++ packages/cesium-map-adapter/src/layer-adapters/BaseAdapter.ts
@@ -2,15 +2,29 @@
  * @module cesium-map-adapter
  */
 import { BaseLayerAdapter, AdapterOptions } from '@nextgis/webmap';
-import { Viewer as TViewer } from 'cesium';
+import { Viewer as TViewer, Event } from 'cesium';
 export type Map = TViewer;
 
 export abstract class BaseAdapter<
   O extends AdapterOptions = AdapterOptions,
   L = any
 > implements BaseLayerAdapter<Map, L, O> {
+  protected onTerrainChange?: () => void;
+  private _terrainProviderChangedListener?: Event.RemoveCallback;
+
   constructor(public map: Map, public options: O) {
-    //
+    if (this.onTerrainChange) {
+      const t = map.scene.terrainProviderChanged;
+      this._terrainProviderChangedListener = t.addEventListener(() => {
+        if (this.onTerrainChange) this.onTerrainChange();
+      });
+    }
+  }
+
+  beforeRemove() {
+    if (this._terrainProviderChangedListener) {
+      this._terrainProviderChangedListener();
+    }
   }
 
   showLayer(layer?: any) {
