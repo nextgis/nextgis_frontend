6a9c7daa4ff690eee095da956503ac6154e5ff5a
Artemiy <rendrom@gmail.com>
2020-06-01 22:37:43 +0800

    feat(ngw-kit): add ngw basemap suppor for url
    not only qms

--- packages/ngw-kit/src/createBasemapLayerAdapter.ts
+++ packages/ngw-kit/src/createBasemapLayerAdapter.ts
@@ -1,18 +1,36 @@
-import { Type, BaseLayerAdapter } from '@nextgis/webmap';
+import { Type, BaseLayerAdapter, TileAdapterOptions } from '@nextgis/webmap';
 import QmsKit from '@nextgis/qms-kit';
 import { GetClassAdapterOptions } from './interfaces';
 
 export async function createBasemapLayerAdapter({
   webMap,
   item,
-}: GetClassAdapterOptions): Promise<Type<BaseLayerAdapter>> {
-  const adapter = Promise.resolve(QmsKit.utils.createQmsAdapter(webMap));
-  adapter.then((x) => {
-    if (x && item && item.basemap_layer && item.basemap_layer.qms) {
-      const qms = JSON.parse(item.basemap_layer.qms);
-      x.prototype.qms = qms;
-      x.prototype.baseLayer = true;
+}: GetClassAdapterOptions): Promise<Type<BaseLayerAdapter> | undefined> {
+  if (item.basemap_layer) {
+    const qms = item.basemap_layer.qms;
+    const url = item.basemap_layer.url;
+    if (qms) {
+      const adapter = Promise.resolve(QmsKit.utils.createQmsAdapter(webMap));
+      adapter.then((x) => {
+        if (x && item) {
+          const qms_ = JSON.parse(qms);
+          x.prototype.qms = qms_;
+          x.prototype.baseLayer = true;
+        }
+      });
+      return adapter;
+    } else if (url) {
+      const TileAdapter = webMap.mapAdapter.layerAdapters.TILE as Type<
+        BaseLayerAdapter<any, any, TileAdapterOptions>
+      >;
+      if (TileAdapter) {
+        class BasemapTileAdapter extends TileAdapter {
+          async addLayer(opt: TileAdapterOptions) {
+            return super.addLayer({ ...opt, url });
+          }
+        }
+        return BasemapTileAdapter;
+      }
     }
-  });
-  return adapter;
+  }
 }
