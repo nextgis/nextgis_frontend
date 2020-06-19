abfa8f77cade76643b85f95f329685efb6cacaf0
Artemiy <rendrom@gmail.com>
2020-06-19 13:27:59 +0800

    docs: create a working api-extractor script

--- scripts/apiDocs.ts
+++ scripts/apiDocs.ts
@@ -1,12 +1,18 @@
-import { Extractor, ExtractorConfig } from '@microsoft/api-extractor';
+import {
+  Extractor,
+  ExtractorConfig,
+  ExtractorLogLevel,
+} from '@microsoft/api-extractor';
 import { execSync } from 'child_process';
 import * as fs from 'fs';
 import * as path from 'path';
 
-async function main() {
-  const reportFolder = path.resolve('input');
-  const reportTempFolder = path.resolve('temp');
+const reportFolder = path.resolve('input');
+const reportTempFolder = path.resolve('temp');
 
+const rimraf = require('rimraf');
+
+async function main() {
   fs.mkdirSync(reportFolder, {
     recursive: true,
   });
@@ -29,6 +35,7 @@ async function main() {
     '@nextgis/leaflet-map-adapter',
     '@nextgis/mapbox-map-adapter',
     '@nextgis/ol-map-adapter',
+    '@nextgis/ngw-orm',
     '@nextgis/cesium-map-adapter',
     '@nextgis/ngw-leaflet',
     '@nextgis/ngw-ol',
@@ -46,61 +53,96 @@ async function main() {
   const packages = Object.keys(workspaces).filter(
     (p) => !skipPackages.includes(p)
   );
-  packages.forEach((packageName) => {
-    const libPath = `packages${packageName.replace('@nextgis', '')}`;
-    const packagePath = `${libPath}/package.json`;
-    const packageJson = require(`../${packagePath}`) as any;
-
-    const config = ExtractorConfig.prepare({
-      packageJson,
-      packageJsonFullPath: path.resolve(`${packagePath}`),
-      configObjectFullPath: path.resolve(`${libPath}`),
-      configObject: {
-        projectFolder: path.resolve(libPath),
-        mainEntryPointFilePath: path.resolve(`${libPath}/lib/index.d.ts`),
-        compiler: {
-          tsconfigFilePath: path.resolve(`${libPath}/tsconfig.json`),
-        },
-        docModel: {
-          enabled: true,
-          apiJsonFilePath: `${reportFolder}/<unscopedPackageName>.api.json`,
-        },
-        apiReport: {
-          enabled: true,
-          reportFolder,
-          reportTempFolder,
-          reportFileName: '<unscopedPackageName>.api.md',
-        },
-      },
-    });
-
-    const result = Extractor.invoke(config, {
-      localBuild: true,
-      messageCallback: (message) => {
-        let loc = '';
-        if (message.sourceFilePath !== undefined) {
-          loc += `${message.sourceFilePath}:`;
-          if (message.sourceFileLine !== undefined) {
-            loc += `${message.sourceFileLine}:`;
-            if (message.sourceFileColumn !== undefined) {
-              loc += `${message.sourceFileColumn}:`;
-            }
-          }
-          loc += ' ';
-        }
-        // tslint:disable-next-line: no-console
-        console.warn(
-          `${loc}(${message.category}) ${message.text} (${message.messageId})`
-        );
-      },
-    });
-    if (!result.succeeded) {
-      throw new Error(`failed to extract api when processing '${packageName}'`);
-    }
-  });
+  for (const packageName of packages) {
+    await extractPkg(packageName);
+  }
 
   // tslint:disable-next-line: no-console
-  console.log(execSync('yarn exec api-documenter markdown').toString());
+  console.log(execSync('yarn docs:md').toString());
+}
+
+async function extractPkg(packageName: string) {
+  const libPath = `packages${packageName.replace('@nextgis', '')}`;
+  const dist = path.resolve(libPath, 'lib');
+  const packagePath = `${libPath}/package.json`;
+  const packageJson = require(`../${packagePath}`) as any;
+  const packageJsonFullPath = path.resolve(`${packagePath}`);
+  const untrimmedFilePath = path.resolve(`${libPath}/lib/dtsRollup/index.d.ts`);
+
+  const config = ExtractorConfig.prepare({
+    packageJson,
+    packageJsonFullPath,
+    configObjectFullPath: path.resolve(`${libPath}`),
+    configObject: {
+      projectFolder: path.resolve(libPath),
+      mainEntryPointFilePath: path.resolve(`${libPath}/lib/index.d.ts`),
+      compiler: {
+        tsconfigFilePath: path.resolve(`${libPath}/tsconfig.json`),
+      },
+      docModel: {
+        enabled: true,
+        apiJsonFilePath: `${reportFolder}/<unscopedPackageName>.api.json`,
+      },
+      tsdocMetadata: {
+        enabled: false,
+      },
+      dtsRollup: {
+        enabled: true,
+        untrimmedFilePath,
+      },
+      apiReport: {
+        enabled: true,
+        reportFolder,
+        reportTempFolder,
+        reportFileName: '<unscopedPackageName>.api.md',
+      },
+      messages: {
+        extractorMessageReporting: {
+          'ae-missing-release-tag': {
+            logLevel: ExtractorLogLevel.None,
+          },
+        },
+      },
+    },
+  });
+
+  const result = Extractor.invoke(config, {
+    localBuild: true,
+    messageCallback: (message) => {
+      let loc = '';
+      if (message.sourceFilePath !== undefined) {
+        loc += `${message.sourceFilePath}:`;
+        if (message.sourceFileLine !== undefined) {
+          loc += `${message.sourceFileLine}:`;
+          if (message.sourceFileColumn !== undefined) {
+            loc += `${message.sourceFileColumn}:`;
+          }
+        }
+        loc += ' ';
+      }
+      console.warn(
+        `${loc}(${message.category}) ${message.text} (${message.messageId})`
+      );
+    },
+  });
+  if (!result.succeeded) {
+    throw new Error(`failed to extract api when processing '${packageName}'`);
+  }
+
+  const dtsRollupExist = path.resolve(dist, 'dtsRollup');
+  if (fs.existsSync(dtsRollupExist)) {
+    const typesPath = path.resolve(libPath, packageJson.types);
+    fs.renameSync(untrimmedFilePath, typesPath);
+
+    const typeFiles = fs.readdirSync(dist);
+    typeFiles.forEach((x) => {
+      const filePath = path.resolve(dist, x);
+      const mainPath = path.resolve(libPath, packageJson.main);
+      if (filePath !== typesPath && filePath !== mainPath) {
+        rimraf.sync(filePath);
+      }
+    });
+  }
 }
 
 main();
