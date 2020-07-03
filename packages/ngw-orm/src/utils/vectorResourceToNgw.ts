7cf18a983403bb66a4d6ab2d9b6e28e4d43fd5ea
Artemiy <rendrom@gmail.com>
2020-07-03 16:02:40 +0800

    wip(ngw-orm): static save method

--- packages/ngw-orm/src/utils/vectorResourceToNgw.ts
+++ packages/ngw-orm/src/utils/vectorResourceToNgw.ts
@@ -9,60 +9,63 @@ import { VectorLayer } from '../repository/VectorLayer';
 
 export interface VectorResourceToNgwOptions {
   resource: ResourceItem;
-  item: VectorLayer;
+  items: VectorLayer[];
 }
 
 export function vectorResourceToNgw(
   opt: VectorResourceToNgwOptions
-): Partial<FeatureItem<GeoJsonProperties, string>> {
-  const geom = prepareGeomToNgw(opt.item.geom);
+): Partial<FeatureItem<GeoJsonProperties, string>>[] {
+  // const features: Partial<FeatureItem<GeoJsonProperties, string>>[] = [];
+  return opt.items.map((item) => {
+    const geom = prepareGeomToNgw(item.geom);
 
-  const fields: GeoJsonProperties = {};
-  const featureLayer = opt.resource.feature_layer as FeatureResource;
-  featureLayer.fields.forEach((x) => {
-    if (x.keyname in opt.item) {
-      // @ts-ignore
-      const property = opt.item[x.keyname];
+    const fields: GeoJsonProperties = {};
+    const featureLayer = opt.resource.feature_layer as FeatureResource;
+    featureLayer.fields.forEach((x) => {
+      if (x.keyname in item) {
+        // @ts-ignore
+        const property = opt.item[x.keyname];
 
-      let value: any;
-      if (property) {
-        if (x.datatype === 'STRING') {
-          value = String(property);
-        } else if (x.datatype === 'BIGINT' || x.datatype === 'INTEGER') {
-          value = parseInt(property, 10);
-        } else if (x.datatype === 'REAL') {
-          value = parseFloat(property);
-        } else if (x.datatype === 'DATE') {
-          let dt: Date | undefined;
-          if (typeof property === 'object') {
-            value = property;
-          } else {
-            if (property instanceof Date) {
-              dt = property;
+        let value: any;
+        if (property) {
+          if (x.datatype === 'STRING') {
+            value = String(property);
+          } else if (x.datatype === 'BIGINT' || x.datatype === 'INTEGER') {
+            value = parseInt(property, 10);
+          } else if (x.datatype === 'REAL') {
+            value = parseFloat(property);
+          } else if (x.datatype === 'DATE') {
+            let dt: Date | undefined;
+            if (typeof property === 'object') {
+              value = property;
             } else {
-              const parse = Date.parse(property);
-              if (parse) {
-                dt = new Date(parse);
+              if (property instanceof Date) {
+                dt = property;
+              } else {
+                const parse = Date.parse(property);
+                if (parse) {
+                  dt = new Date(parse);
+                }
               }
-            }
-            if (dt) {
-              value = {
-                year: dt.getFullYear(),
-                month: dt.getMonth(),
-                day: dt.getDay(),
-              };
+              if (dt) {
+                value = {
+                  year: dt.getFullYear(),
+                  month: dt.getMonth(),
+                  day: dt.getDay(),
+                };
+              }
             }
           }
         }
+        // @ts-ignore
+        fields[x.keyname] = value || null;
       }
-      // @ts-ignore
-      fields[x.keyname] = value || null;
-    }
-  });
+    });
 
-  const feature: Partial<FeatureItem<GeoJsonProperties, string>> = {
-    fields,
-    geom,
-  };
-  return feature;
+    return {
+      id: item.id,
+      fields,
+      geom,
+    };
+  });
 }
