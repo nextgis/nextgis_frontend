3775bbc07afcd2e132364cce41e29253bae0796e
Artemiy <rendrom@gmail.com>
2020-07-18 22:07:18 +0800

    wip: vue libs to rollup

--- packages/ngw-orm/src/vector-layer-utils/vectorResourceToNgw.ts
+++ packages/ngw-orm/src/vector-layer-utils/vectorResourceToNgw.ts
@@ -1,10 +1,10 @@
-import { GeoJsonProperties, GeoJsonObject } from 'geojson';
+import { GeoJsonProperties, Geometry } from 'geojson';
+import { prepareFieldsToNgw } from '@nextgis/ngw-kit';
 import {
   ResourceItem,
   FeatureResource,
   FeatureItem,
 } from '@nextgis/ngw-connector';
-import { prepareGeomToNgw } from './prepareGeomToNgw';
 import { VectorLayer } from '../repository/VectorLayer';
 
 export interface VectorResourceToNgwOptions {
@@ -14,52 +14,12 @@ export interface VectorResourceToNgwOptions {
 
 export function vectorResourceToNgw(
   opt: VectorResourceToNgwOptions
-): Partial<FeatureItem<GeoJsonProperties, GeoJsonObject>>[] {
+): Partial<FeatureItem<GeoJsonProperties, Geometry>>[] {
   // const features: Partial<FeatureItem<GeoJsonProperties, string>>[] = [];
   return opt.items.map((item) => {
-    const geom = prepareGeomToNgw(item.geom);
-    const fields: GeoJsonProperties = {};
+    const geom = item.geom;
     const featureLayer = opt.resource.feature_layer as FeatureResource;
-    featureLayer.fields.forEach((x) => {
-      if (x.keyname in item) {
-        const keyname = x.keyname as keyof VectorLayer;
-        const property = item[keyname];
-        let value: any;
-        if (property !== undefined) {
-          if (x.datatype === 'STRING') {
-            value = String(property);
-          } else if (x.datatype === 'BIGINT' || x.datatype === 'INTEGER') {
-            value =
-              typeof property === 'string' ? parseInt(property, 10) : property;
-          } else if (x.datatype === 'REAL') {
-            value =
-              typeof property === 'string' ? parseFloat(property) : property;
-          } else if (x.datatype === 'DATE') {
-            let dt: Date | undefined;
-            if (typeof property === 'object') {
-              value = property;
-            } else {
-              if (property instanceof Date) {
-                dt = property;
-              } else {
-                const parse = Date.parse(String(property));
-                if (parse) {
-                  dt = new Date(parse);
-                }
-              }
-              if (dt) {
-                value = {
-                  year: dt.getFullYear(),
-                  month: dt.getMonth(),
-                  day: dt.getDay(),
-                };
-              }
-            }
-          }
-        }
-        fields[x.keyname] = value ?? null;
-      }
-    });
+    const fields = prepareFieldsToNgw(item, featureLayer.fields);
     return {
       id: item.id,
       fields,
--- packages/vuetify-ngw-components/README.md
+++ packages/vuetify-ngw-components/README.md
@@ -4,11 +4,8 @@ Vuetify components for NGW Frontend
 
 ## LayersTree
 
-
-
 ## Commercial support
 
-Need to fix a bug or add a feature to @nextgis/icons? We provide custom development and support for this software. [Contact us](http://nextgis.com/contact/) to discuss options!
+Need to fix a bug or add a feature to @nextgis/vuetify-ngw-components? We provide custom development and support for this software. [Contact us](http://nextgis.com/contact/) to discuss options!
 
 [![http://nextgis.com](http://nextgis.ru/img/nextgis.png)](http://nextgis.com)
-
