ecd3b3a92991f0f9ae0ed77a4464f19fc1d8ff3b
Artemiy <rendrom@gmail.com>
2020-07-23 12:15:53 +0800

    build: vue-ngw-map components to rollup

---
+++ packages/vue-ngw-ol/src/VueNgwOl.ts
@@ -0,0 +1,17 @@
+// import '@nextgis/control-container/lib/control-container.css';
+import 'ol/ol.css';
+
+import MapAdapter from '@nextgis/ol-map-adapter';
+import { VueNgwMap } from '@nextgis/vue-ngw-map';
+import { NgwMap } from '@nextgis/ngw-map';
+
+import Map from 'ol/Map';
+
+import { Mixins, Prop, Component } from 'vue-property-decorator';
+
+@Component
+export class VueNgwOl extends Mixins(VueNgwMap) implements VueNgwMap<Map> {
+  @Prop({ type: Object, default: () => new MapAdapter() })
+  readonly mapAdapter!: MapAdapter;
+  ngwMap: NgwMap<Map> | null = null;
+}
