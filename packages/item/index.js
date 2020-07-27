504013e3db832138b72f6a6359175e771e937693
Artemiy <rendrom@gmail.com>
2020-07-27 17:31:24 +0800

    fix(build): fix item index.js error

--- packages/item/index.js
+++ packages/item/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./lib/items.cjs.prod.js');
+  module.exports = require('./lib/item.cjs.prod.js');
 } else {
-  module.exports = require('./lib/items.cjs.js');
+  module.exports = require('./lib/item.cjs.js');
 }
