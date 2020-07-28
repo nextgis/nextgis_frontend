29a437d942418ab16a9e39d7b51b8738c10090b0
Artemiy <rendrom@gmail.com>
2020-07-28 21:07:44 +0800

    fix: improve node/browser splitting

--- packages/ngw-kit/src/utils/createPopupContent.ts
+++ packages/ngw-kit/src/utils/createPopupContent.ts
@@ -1,28 +1,32 @@
 import { Feature } from 'geojson';
 import { ResourceItem } from '@nextgis/ngw-connector';
-import { create } from '@nextgis/dom';
 
 export function createPopupContent(
   feature: Feature,
   item?: ResourceItem
 ): HTMLElement | string {
-  const element = create('div');
-  if (item) {
-    item.feature_layer?.fields.forEach((x) => {
-      if (x.grid_visibility) {
-        const value = feature.properties && feature.properties[x.keyname];
-        if (value) {
-          const propElem = create('div', null, element);
-          propElem.innerHTML = `<span>${x.display_name}</span>: ${value}<span></span>`;
+  if (__BROWSER__) {
+    const element = document.createElement('div');
+    if (item) {
+      item.feature_layer?.fields.forEach((x) => {
+        if (x.grid_visibility) {
+          const value = feature.properties && feature.properties[x.keyname];
+          if (value) {
+            const propElem = document.createElement('div');
+            element.appendChild(propElem);
+            propElem.innerHTML = `<span>${x.display_name}</span>: ${value}<span></span>`;
+          }
         }
+      });
+    } else if (feature.properties) {
+      for (const p in feature.properties) {
+        const propElem = document.createElement('div');
+        element.appendChild(propElem);
+        propElem.innerHTML = `<span>${p}</span>: ${feature.properties[p]}<span></span>`;
       }
-    });
-  } else if (feature.properties) {
-    for (const p in feature.properties) {
-      const propElem = create('div', null, element);
-      propElem.innerHTML = `<span>${p}</span>: ${feature.properties[p]}<span></span>`;
     }
+    return element;
+  } else {
+    return '';
   }
-
-  return element;
 }
