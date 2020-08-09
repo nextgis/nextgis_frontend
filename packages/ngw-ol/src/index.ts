655fe30171a2d2f6255faa58067fce65b7018bbf
Artemiy <rendrom@gmail.com>
2020-08-09 20:56:04 +0800

    docs: update ngw-map packages readme

--- packages/ngw-ol/src/index.ts
+++ packages/ngw-ol/src/index.ts
@@ -1,3 +1,11 @@
+/**
+ * Single-file bundle for rapid deployment of {@link https://openlayers.org/ | OpenLayers }-based web-gis applications with NextGIS services
+ *
+ * @remarks
+ * Styles images and other assets are already in bundle, you don't need to include anything except one JS file!
+ *
+ * @packageDocumentation
+ */
 import 'ol/ol.css';
 import '@nextgis/control-container/lib/control-container.css';
 import { NgwMap, NgwMapOptions } from '@nextgis/ngw-map';
