033f4827e32a9ef9cb764e80cde6944534a5f923
Artemiy <rendrom@gmail.com>
2020-08-12 13:07:21 +0800

    feat(utils): add DebounceDecorator

--- packages/utils/src/debounce.ts
+++ packages/utils/src/debounce.ts
@@ -1,11 +1,49 @@
+// based on https://github.com/bvaughn/debounce-decorator
+
 export function debounce<T extends (...args: any[]) => void>(
   cb: T,
   wait = 10
-): T {
-  let h = 0;
-  const callable = (...args: any) => {
-    clearTimeout(h);
-    h = window.setTimeout(() => cb(...args), wait);
+): T & { clear: () => void } {
+  let timeoutId: any;
+
+  function wrapper(this: any, ...args: any[]) {
+    wrapper.clear();
+
+    timeoutId = setTimeout(() => {
+      timeoutId = null;
+      cb.apply(this, args);
+    }, wait);
+  }
+
+  wrapper.clear = function () {
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+      timeoutId = null;
+    }
   };
-  return (callable as any) as T;
+
+  return wrapper as T & { clear: () => void };
+}
+
+export function DebounceDecorator(wait = 10) {
+  return function (
+    target: unknown,
+    key: string,
+    descriptor: PropertyDescriptor
+  ): PropertyDescriptor {
+    return {
+      configurable: true,
+      enumerable: descriptor.enumerable,
+      get: function getter() {
+        // Attach this function to the instance (not the class)
+        Object.defineProperty(this, key, {
+          configurable: true,
+          enumerable: descriptor.enumerable,
+          value: debounce(descriptor.value, wait),
+        });
+        // @ts-ignore
+        return this[key];
+      },
+    };
+  };
 }
