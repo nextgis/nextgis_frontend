81619b29d60cb7ad7da7a404e2ca48b7624a0635
Artemiy <rendrom@gmail.com>
2020-08-15 12:06:11 +0800

    feat(ngw-orm): update VectorLayer.toTypescript

---
+++ packages/utils/src/string/camelize.ts
@@ -0,0 +1,9 @@
+export function camelize(str: string): string {
+  return str
+    .replace(/^[_.\- ]+/, '')
+    .toLocaleLowerCase()
+    .replace(/[_.\- ]+([\p{Alpha}\p{N}_]|$)/gu, (_, p1) =>
+      p1.toLocaleUpperCase()
+    )
+    .replace(/\d+([\p{Alpha}\p{N}_]|$)/gu, (m) => m.toLocaleUpperCase());
+}
