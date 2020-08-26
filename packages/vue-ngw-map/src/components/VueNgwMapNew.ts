a60db01367add11783ae121d2b795bbdec88fd1d
Artemiy <rendrom@gmail.com>
2020-08-26 21:50:19 +0800

    wip(vue): new style for vue-ngw-map

---
+++ packages/vue-ngw-map/src/components/VueNgwMapNew.ts
@@ -0,0 +1,78 @@
+import Vue, { VNode, VNodeData, CreateElement, Component } from 'vue';
+// import { Prop, Vue } from 'vue-property-decorator';
+// import Component from 'vue-class-component';
+import { MapAdapter } from '@nextgis/webmap';
+import { NgwMap, NgwMapOptions } from '@nextgis/ngw-map';
+
+import NgwConnector from '@nextgis/ngw-connector';
+
+export interface VueNgwMapData<M = any> {
+  ngwMap: NgwMap<M>;
+  ready: boolean;
+}
+
+// interface Methods {}
+// interface Computed {}
+export interface VueNgwMapProps {
+  mapAdapter: MapAdapter;
+  fullFilling: boolean;
+  connector: NgwConnector;
+  baseUrl: string;
+  qmsId: string;
+  webMapId: string;
+  mapOptions: NgwMapOptions;
+}
+
+export const VueNgwMap = Vue.extend<VueNgwMapData, any, any, VueNgwMapProps>({
+  props: {
+    mapAdapter: Object,
+    fullFilling: Boolean,
+    connector: Object,
+    baseUrl: String,
+    qmsId: String,
+    webMapId: String,
+    mapOptions: Object,
+  },
+
+  data: {
+    ngwMap: {} as NgwMap,
+    ready: false,
+  },
+
+  mounted(): void {
+    this.ngwMap = new NgwMap(this.$props.mapAdapter, {
+      ...this.$props,
+      ...this.$props.mapOptions,
+      target: this.$el as HTMLElement,
+    });
+    this.ngwMap.onLoad().then(() => {
+      this.$nextTick().then(() => {
+        this.ready = true;
+        this.$emit('load', this.ngwMap);
+      });
+    });
+  },
+  beforeDestroy(): void {
+    if (this.ngwMap) {
+      this.ngwMap.destroy();
+    }
+  },
+  render(h: CreateElement): VNode {
+    const staticStyle: { [param: string]: string } = {
+      zIndex: '0',
+    };
+    if (this.$props.fullFilling) {
+      staticStyle.width = '100%';
+      staticStyle.height = '100%';
+    }
+
+    const data: VNodeData = {
+      staticClass: 'vue-ngw-map',
+      staticStyle,
+      // 'class': this.classes,
+      attrs: { 'data-app': true },
+      // domProps: { id: this.id }
+    };
+    return this.ready ? h('div', data, this.$slots.default) : h('div', data);
+  },
+});
