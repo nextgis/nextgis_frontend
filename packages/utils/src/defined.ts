39d1dee32c1e5f21c079d8e8e393aa31c0794225
Artemiy <rendrom@gmail.com>
2020-08-28 20:59:24 +0800

    feat(utils): add `full` method

--- packages/utils/src/defined.ts
+++ packages/utils/src/defined.ts
@@ -1,20 +1,25 @@
 /**
  * from https://github.com/CesiumGS/cesium/blob/master/Source/Core/defined.js
  *
- * @exports defined
- *
  * @param val - The object.
  * @returns Returns true if the object is defined, returns false otherwise.
  *
  * @example
  * ```javascript
  * if (defined(positions)) {
- *      doSomething();
+ *   doSomething();
  * } else {
- *      doSomethingElse();
+ *   doSomethingElse();
  * }
  * ```
  */
 export function defined(val: unknown): val is boolean {
   return val !== undefined && val !== null;
+}
+
+export function full(val: unknown): val is boolean {
+  if (typeof val === 'string') {
+    return !!val;
+  }
+  return defined(val);
 }
