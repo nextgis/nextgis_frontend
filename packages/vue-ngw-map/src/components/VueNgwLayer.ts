f9cb1fd2e5b4ba11faf4e22ee76f0e9576de5dac
Artemiy <rendrom@gmail.com>
2020-08-31 17:50:38 +0800

    feat(vue): add GeojsonLayer

--- packages/vue-ngw-map/src/components/VueNgwLayer.ts
+++ packages/vue-ngw-map/src/components/VueNgwLayer.ts
@@ -1,9 +1,14 @@
 import Vue, { VNode, CreateElement } from 'vue';
 import Component from 'vue-class-component';
-import { Prop } from 'vue-property-decorator';
-import { AdapterOptions, LayerAdapters, LayerAdapter } from '@nextgis/webmap';
-import VueNgwMap from './VueNgwMap';
-import { findNgwMapParent, propsBinder } from '../utils';
+import { Prop, InjectReactive } from 'vue-property-decorator';
+import {
+  AdapterOptions,
+  LayerAdapters,
+  LayerAdapter,
+  LayerAdapterDefinition,
+} from '@nextgis/webmap';
+import { NgwMap } from '@nextgis/ngw-map';
+import { propsBinder } from '../utils';
 
 @Component
 export class VueNgwLayer extends Vue {
@@ -11,21 +16,28 @@ export class VueNgwLayer extends Vue {
   @Prop({ type: Object, default: () => ({}) }) adapterOptions!: AdapterOptions;
   @Prop({ type: Boolean, default: false }) fit!: boolean;
 
-  name = 'vue-ngw-layer';
+  @InjectReactive() readonly ngwMap!: NgwMap;
 
-  parentContainer!: VueNgwMap;
+  name = 'vue-ngw-layer';
 
   layer?: LayerAdapter;
 
   beforeDestroy(): void {
-    if (this.parentContainer.ngwMap && this.layer) {
-      this.parentContainer.ngwMap.removeLayer(this.layer);
+    if (this.ngwMap && this.layer) {
+      this.ngwMap.removeLayer(this.layer);
       this.layer = undefined;
     }
   }
 
+  addLayer(
+    adapter: LayerAdapterDefinition,
+    options: AdapterOptions = {}
+  ): Promise<LayerAdapter> {
+    return this.ngwMap.addLayer(adapter, options);
+  }
+
   async setLayer(): Promise<void> {
-    const ngwMap = this.parentContainer.ngwMap;
+    const ngwMap = this.ngwMap;
     const layer = this.layer;
     let order: number | undefined;
     if (ngwMap) {
@@ -39,16 +51,14 @@ export class VueNgwLayer extends Vue {
       }
 
       const fit = this.$props.fit;
-      this.layer = await ngwMap.addLayer(this.$props.adapter, {
+      this.layer = await this.addLayer(this.$props.adapter, {
         fit,
-        adapterOptions,
+        ...adapterOptions,
       });
     }
   }
 
   async mounted(): Promise<void> {
-    this.parentContainer = findNgwMapParent(this.$parent);
-
     await this.setLayer();
 
     propsBinder(this, this.$props);
