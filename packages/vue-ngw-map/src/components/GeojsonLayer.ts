0871ba3318b82296020c12ee3fb20432e9f107b9
Artemiy <rendrom@gmail.com>
2020-09-01 13:53:14 +0800

    feat(vue): GeojsonLayer selected model and onClick

--- packages/vue-ngw-map/src/components/GeojsonLayer.ts
+++ packages/vue-ngw-map/src/components/GeojsonLayer.ts
@@ -1,19 +1,22 @@
-import { GeoJsonObject } from 'geojson';
+import { GeoJsonObject, Feature } from 'geojson';
 import Component from 'vue-class-component';
-import { Prop, Mixins, Watch } from 'vue-property-decorator';
+import { Prop, Mixins, Watch, Emit, Model } from 'vue-property-decorator';
 
 import {
   LayerAdapters,
   LayerAdapter,
   VectorLayerAdapter,
   AdapterOptions,
+  OnLayerClickOptions,
 } from '@nextgis/webmap';
 import { VueNgwLayer } from './VueNgwLayer';
 
 @Component
 export class GeojsonLayer extends Mixins(VueNgwLayer) {
+  @Model('change') readonly selected!: number[];
   @Prop({ type: String, default: 'GEOJSON' }) adapter!: keyof LayerAdapters;
   @Prop({ default: 'GEOJSON' }) data!: GeoJsonObject;
+  @Prop({ default: 'id' }) readonly idField!: string;
 
   name = 'geojson-layer';
 
@@ -28,10 +31,56 @@ export class GeojsonLayer extends Mixins(VueNgwLayer) {
     }
   }
 
+  @Watch('selected')
+  onSelectedChange(): void {
+    if (this.layer && this.layer.select) {
+      this.layer.select((e) => {
+        if (e.feature && e.feature.properties) {
+          return (
+            this.selected.indexOf(e.feature.properties[this.idField]) !== -1
+          );
+        }
+        return false;
+      });
+    }
+  }
+
+  @Emit('click')
+  onLayerClick(opt: OnLayerClickOptions): OnLayerClickOptions {
+    this.selectedChange();
+    return opt;
+  }
+
+  @Emit('change')
+  selectedChange(): (string | number)[] {
+    const selected: (number | string)[] = [];
+    if (this.layer && this.layer.getSelected) {
+      this.layer.getSelected().forEach((x) => {
+        if (x.feature) {
+          let key = x.feature.properties && x.feature.properties[this.idField];
+          key = key !== undefined ? key : x.feature.id;
+          if (key !== undefined) {
+            selected.push(key);
+          }
+        }
+      });
+    }
+    return selected;
+  }
+
   addLayer(
     adapter: 'GEOJSON',
     options: AdapterOptions = {}
   ): Promise<LayerAdapter> {
-    return this.ngwMap.addLayer(adapter, { ...options, data: this.data });
+    return this.ngwMap
+      .addLayer(adapter, {
+        ...options,
+        data: this.data,
+        onLayerClick: this.onLayerClick,
+      })
+      .then((layer) => {
+        this.onSelectedChange();
+        return layer;
+      });
   }
 }
