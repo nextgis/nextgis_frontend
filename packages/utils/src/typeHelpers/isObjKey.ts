45e175b8b49511f619f3a105e40b975fcc9a2305
Artemiy <rendrom@gmail.com>
2020-09-23 11:35:40 +0800

    refactor(utils): rename keyInObj to isObjKey

---
+++ packages/utils/src/typeHelpers/isObjKey.ts
@@ -0,0 +1,22 @@
+type Obj = Record<string | number, any>;
+
+export function isObjKey<O extends Obj = Obj>(
+  obj: O,
+  key: unknown
+): key is keyof O {
+  if (typeof key === 'string' || typeof key === 'number') {
+    return key in obj;
+  }
+  return false;
+}
+
+/**
+ *
+ * @deprecated - use isObjectKey instead
+ */
+export function keyInObj<O extends Obj = Obj>(
+  obj: O,
+  key: unknown
+): key is keyof O {
+  return isObjKey(obj, key);
+}
