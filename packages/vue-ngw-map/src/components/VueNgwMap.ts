2f716b3617f1de7e25366c02af75ba76b64268c1
Artemiy <rendrom@gmail.com>
2020-09-25 14:13:28 +0800

    feat(vue): VueNgwMap bounds param watch

--- packages/vue-ngw-map/src/components/VueNgwMap.ts
+++ packages/vue-ngw-map/src/components/VueNgwMap.ts
@@ -1,20 +1,22 @@
 import { VNode, VNodeData, CreateElement } from 'vue';
-import { Prop, Vue } from 'vue-property-decorator';
+import { Prop, Vue, Watch } from 'vue-property-decorator';
 import Component from 'vue-class-component';
 import { MapAdapter } from '@nextgis/webmap';
+import { LngLatBoundsArray } from '@nextgis/utils';
 import { NgwMap, NgwMapOptions } from '@nextgis/ngw-map';
 
 import NgwConnector from '@nextgis/ngw-connector';
 
 @Component
 export class VueNgwMap<M = any> extends Vue {
-  @Prop({ type: Object }) mapAdapter!: MapAdapter;
-  @Prop({ type: Boolean }) fullFilling!: boolean;
-  @Prop({ type: NgwConnector }) connector!: NgwConnector;
-  @Prop({ type: String }) baseUrl!: string;
-  @Prop({ type: Number }) qmsId!: string;
-  @Prop({ type: String }) webMapId!: string;
-  @Prop({ type: Object }) mapOptions!: NgwMapOptions;
+  @Prop({ type: Object }) readonly mapAdapter!: MapAdapter;
+  @Prop({ type: Boolean }) readonly fullFilling!: boolean;
+  @Prop({ type: NgwConnector }) readonly connector!: NgwConnector;
+  @Prop({ type: String }) readonly baseUrl!: string;
+  @Prop({ type: Number }) readonly qmsId!: string;
+  @Prop({ type: String }) readonly webMapId!: string;
+  @Prop({ type: Object }) readonly mapOptions!: NgwMapOptions;
+  @Prop({ type: Array }) readonly bounds!: LngLatBoundsArray;
 
   // @ProvideReactive() ngwMap!: NgwMap<M>;
   ngwMap!: NgwMap<M>;
@@ -22,14 +24,21 @@ export class VueNgwMap<M = any> extends Vue {
   name = 'vue-ngw-map';
   ready = false;
 
+  @Watch('bounds')
+  onBoundsChange(bounds: LngLatBoundsArray): void {
+    if (this.ngwMap) {
+      this.ngwMap.fitBounds(bounds);
+    }
+  }
+
   getMapOptions(): NgwMapOptions {
     return this.mapOptions;
   }
 
   mounted(): void {
     this.ngwMap = new NgwMap(this.mapAdapter, {
-      ...this.$props,
       ...this.getMapOptions(),
+      ...this.$props,
       target: this.$el as HTMLElement,
     });
     this.ngwMap.onLoad().then(() => {
