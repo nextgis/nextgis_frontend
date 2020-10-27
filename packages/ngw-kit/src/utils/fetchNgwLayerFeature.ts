e70622580a46f93cd321f66257e277403a223dc7
Artemiy <rendrom@gmail.com>
2020-10-27 15:17:20 +0800

    refactor(nge-kit): rename `get-` feature methods to `fetch-`

---
+++ packages/ngw-kit/src/utils/fetchNgwLayerFeature.ts
@@ -0,0 +1,32 @@
+import {
+  Geometry,
+  Feature,
+} from 'geojson';
+
+import NgwConnector, {
+
+} from '@nextgis/ngw-connector';
+import CancelablePromise from '@nextgis/cancelable-promise';
+
+import {
+
+  NgwFeatureRequestOptions,
+} from '../interfaces';
+import { fetchNgwLayerItem } from './fetchNgwLayerItem';
+import { createGeoJsonFeature } from './featureLayerUtils';
+
+
+export function fetchNgwLayerFeature<
+  G extends Geometry = Geometry,
+  P extends Record<string, any> = Record<string, any>
+>(
+  options: {
+    resourceId: number;
+    featureId: number;
+    connector: NgwConnector;
+  } & NgwFeatureRequestOptions
+): CancelablePromise<Feature<G, P>> {
+  return fetchNgwLayerItem<G, P>(options).then((item) => {
+    return createGeoJsonFeature<G, P>(item);
+  });
+}
