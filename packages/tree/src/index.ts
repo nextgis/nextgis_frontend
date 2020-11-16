e3de410db9de6303effd99d2f81aa15aa8420005
Artemiy <rendrom@gmail.com>
2020-11-16 15:08:11 +0800

    docs(tree): add usage examples

--- packages/tree/src/index.ts
+++ packages/tree/src/index.ts
@@ -1,1 +1,11 @@
-export * from './treeFilter';
+import { treeEvery, treeFilter, treeFind, treeSome } from './treeFilter';
+
+export { treeEvery, treeFilter, treeFind, treeSome };
+
+// shortcut to use like `Tree.find`
+export {
+  treeEvery as every,
+  treeFilter as filter,
+  treeFind as find,
+  treeSome as some,
+};
