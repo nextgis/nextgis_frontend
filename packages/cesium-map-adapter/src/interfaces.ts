a2df497be8adbd66acdc8c2d41ae79c23a71ffe4
Artemiy <rendrom@gmail.com>
2020-11-19 15:31:45 +0800

    types(cesium-map-adapter): define cesium adapter map click event source

---
+++ packages/cesium-map-adapter/src/interfaces.ts
@@ -0,0 +1,13 @@
+import { LngLatArray, MapClickEvent } from '@nextgis/webmap';
+import { Cartesian2, Cartesian3 } from 'cesium';
+
+export interface CesiumMapClickEvent {
+  position: Cartesian2;
+}
+
+export interface CesiumAdapterMapClickEvent extends MapClickEvent {
+  source: CesiumMapClickEvent & {
+    pickedPosition?: Cartesian3;
+    pickedPositionLngLat?: LngLatArray;
+  };
+}
