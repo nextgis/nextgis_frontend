4b4a89de742a71f8b55c9677db92ac9713af081d
Artemiy <rendrom@gmail.com>
2021-01-06 14:58:37 +0800

    feat(tree): TreeHelper

--- packages/tree/src/index.ts
+++ packages/tree/src/index.ts
@@ -1,8 +1,9 @@
+import { Tree } from './Tree';
 import { treeEvery, treeFilter, treeFind, treeSome } from './treeFilter';
 
-export { treeEvery, treeFilter, treeFind, treeSome };
+export { treeEvery, treeFilter, treeFind, treeSome, Tree };
 
-// shortcut to use like `Tree.find`
+// shortcut to use like `Tree.find` from global scope
 export {
   treeEvery as every,
   treeFilter as filter,
