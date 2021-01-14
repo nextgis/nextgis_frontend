f7e9ed044ed39fbc95c73ad381560e692dda6046
Artemiy <rendrom@gmail.com>
2021-01-14 17:08:51 +0800

    perf(leaflet): abort xhr tile loading on setView

--- packages/leaflet-map-adapter/src/layer-adapters/RemoteTileLayer.ts
+++ packages/leaflet-map-adapter/src/layer-adapters/RemoteTileLayer.ts
@@ -1,40 +1,65 @@
-import { TileLayerOptions } from 'leaflet';
+import { DomUtil, TileLayerOptions, Util } from 'leaflet';
 import { callAjax } from './layersUtility';
 
-export class RemoteTileLayer<O extends TileLayerOptions = TileLayerOptions> {
-  options!: O;
+type Constructor = new (...args: any[]) => {};
 
-  // _removeTile(key: any) {
-  //   // @ts-ignore
-  //   super._removeTile(key);
-  // }
+export function makeRemote<
+  TBase extends Constructor,
+  O extends TileLayerOptions = TileLayerOptions
+>(Base: TBase) {
+  return class RemoteTileLayer extends Base {
+    options!: O;
 
-  createTile(
-    coords: Record<string, unknown>,
-    done: (error: any, tile: HTMLImageElement) => void
-  ): HTMLImageElement {
-    // @ts-ignore
-    const url = this.getTileUrl(coords);
-
-    const tile = document.createElement('img');
-    callAjax(
-      url,
-      (response) => {
-        tile.src = response;
-        done(null, tile);
-      },
+    createTile(
+      coords: Record<string, unknown>,
+      done: (error: any, tile: HTMLImageElement) => void
+    ): HTMLImageElement {
       // @ts-ignore
-      this.options.headers
-    );
+      const url = this.getTileUrl(coords);
 
-    if (this.options.crossOrigin || this.options.crossOrigin === '') {
-      tile.crossOrigin =
-        this.options.crossOrigin === true ? '' : this.options.crossOrigin;
+      const tile = document.createElement('img');
+      (tile as any).abort = callAjax(
+        url,
+        (response) => {
+          tile.src = response;
+          done(null, tile);
+        },
+        // @ts-ignore
+        this.options.headers
+      );
+
+      if (this.options.crossOrigin || this.options.crossOrigin === '') {
+        tile.crossOrigin =
+          this.options.crossOrigin === true ? '' : this.options.crossOrigin;
+      }
+
+      tile.alt = '';
+      tile.setAttribute('role', 'presentation');
+
+      return tile;
     }
 
-    tile.alt = '';
-    tile.setAttribute('role', 'presentation');
+    _abortLoading() {
+      // @ts-ignore
+      const tiles = this._tiles;
+      // @ts-ignore
+      const tileZoom = this._tileZoom;
+      for (let i in tiles) {
+        if (tiles[i].coords.z !== tileZoom) {
+          const tile = tiles[i].el;
 
-    return tile;
-  }
+          tile.onload = Util.falseFn;
+          tile.onerror = Util.falseFn;
+          if (!tile.complete) {
+            if (tile.abort) {
+              tile.abort();
+            }
+            tile.src = Util.emptyImageUrl;
+            DomUtil.remove(tile);
+            delete tiles[i];
+          }
+        }
+      }
+    }
+  };
 }
