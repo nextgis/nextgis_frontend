35a0d8b8bc94fba336aa32fc6ceaaac364015d78
Artemiy <rendrom@gmail.com>
2021-01-31 18:38:03 +0800

    feat(utils): add function to get coordinates from bbox

--- packages/utils/src/geom/getBoundsPolygon.ts
+++ packages/utils/src/geom/getBoundsPolygon.ts
@@ -5,16 +5,20 @@ import { LngLatBoundsArray } from './interfaces';
  * @public
  */
 export function getBoundsPolygon(b: LngLatBoundsArray): Polygon {
+  const polygon: Polygon = {
+    type: 'Polygon',
+    coordinates: [getBoundsCoordinates(b)],
+  };
+  return polygon;
+}
+
+export function getBoundsCoordinates(b: LngLatBoundsArray): number[][] {
   const westNorth = [b[0], b[1]];
   const eastNorth = [b[2], b[1]];
   const eastSouth = [b[2], b[3]];
   const westSouth = [b[0], b[3]];
 
-  const polygon: Polygon = {
-    type: 'Polygon',
-    coordinates: [[westNorth, eastNorth, eastSouth, westSouth, westNorth]],
-  };
-  return polygon;
+  return [westNorth, eastNorth, eastSouth, westSouth, westNorth];
 }
 
 /**
