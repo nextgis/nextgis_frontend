a0342b61875b6224096f7b5a9bfa018856e1bf50
Artemiy <rendrom@gmail.com>
2021-03-04 19:56:11 +0800

    types: resolve some types error

--- packages/ngw-kit/src/utils/featureLayerUtils.ts
+++ packages/ngw-kit/src/utils/featureLayerUtils.ts
@@ -291,62 +291,64 @@ export function fetchNgwLayerItemsRequest<
   }) as CancelablePromise<FeatureItem<P, G>[]>;
 }
 
-export function prepareFieldsToNgw<T extends Record<string, unknown>>(
+export function prepareFieldsToNgw<T extends GeoJsonProperties>(
   item: T,
   resourceFields: Pick<FeatureLayerField, 'keyname' | 'datatype'>[]
 ): Record<keyof T, any> {
   const fields = {} as Record<keyof T, any>;
-  resourceFields.forEach((x) => {
-    if (x.keyname in item) {
-      const keyname = x.keyname as keyof T;
-      const prop = item[keyname];
-      let value: any;
-      if (prop !== undefined) {
-        if (x.datatype === 'STRING') {
-          value = prop ? String(prop) : null;
-          // TODO: remove after v 3.0.0. For backward compatibility
-          if (value === 'null') {
-            value = null;
-          }
-        } else if (x.datatype === 'BIGINT' || x.datatype === 'INTEGER') {
-          value = typeof prop === 'string' ? parseInt(prop, 10) : prop;
-        } else if (x.datatype === 'REAL') {
-          value = typeof prop === 'string' ? parseFloat(prop) : prop;
-        } else if (x.datatype === 'BOOLEAN') {
-          value =
-            typeof prop === 'boolean' || typeof prop === 'number'
-              ? Number(!!prop)
-              : null;
-        } else if (x.datatype === 'DATE' || x.datatype === 'DATETIME') {
-          let dt: Date | undefined;
-          if (typeof prop === 'object' && !((prop as any) instanceof Date)) {
-            value = prop;
-          } else {
-            if ((prop as any) instanceof Date) {
-              dt = prop as any;
-            } else {
-              const parse = Date.parse(String(prop));
-              if (parse) {
-                dt = new Date(parse);
-              }
+  if (item) {
+    resourceFields.forEach((x) => {
+      if (x.keyname in item) {
+        const keyname = x.keyname;
+        const prop = item[keyname];
+        let value: any;
+        if (prop !== undefined) {
+          if (x.datatype === 'STRING') {
+            value = prop ? String(prop) : null;
+            // TODO: remove after v 3.0.0. For backward compatibility
+            if (value === 'null') {
+              value = null;
             }
-            if (dt) {
-              value = {
-                year: dt.getFullYear(),
-                month: dt.getMonth(),
-                day: dt.getDay(),
-              };
-              if (x.datatype === 'DATETIME') {
-                value.hour = dt.getHours();
-                value.minute = dt.getMinutes();
-                value.second = dt.getSeconds();
+          } else if (x.datatype === 'BIGINT' || x.datatype === 'INTEGER') {
+            value = typeof prop === 'string' ? parseInt(prop, 10) : prop;
+          } else if (x.datatype === 'REAL') {
+            value = typeof prop === 'string' ? parseFloat(prop) : prop;
+          } else if (x.datatype === 'BOOLEAN') {
+            value =
+              typeof prop === 'boolean' || typeof prop === 'number'
+                ? Number(!!prop)
+                : null;
+          } else if (x.datatype === 'DATE' || x.datatype === 'DATETIME') {
+            let dt: Date | undefined;
+            if (typeof prop === 'object' && !((prop as any) instanceof Date)) {
+              value = prop;
+            } else {
+              if ((prop as any) instanceof Date) {
+                dt = prop as any;
+              } else {
+                const parse = Date.parse(String(prop));
+                if (parse) {
+                  dt = new Date(parse);
+                }
+              }
+              if (dt) {
+                value = {
+                  year: dt.getFullYear(),
+                  month: dt.getMonth(),
+                  day: dt.getDay(),
+                };
+                if (x.datatype === 'DATETIME') {
+                  value.hour = dt.getHours();
+                  value.minute = dt.getMinutes();
+                  value.second = dt.getSeconds();
+                }
               }
             }
           }
         }
+        fields[keyname as keyof T] = value ?? null;
       }
-      fields[keyname] = value ?? null;
-    }
-  });
+    });
+  }
   return fields;
 }
