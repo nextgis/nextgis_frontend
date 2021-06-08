8117938b0e13d389df84705a5905042710e740a0
Artemiy Doroshkov <rendrom@gmail.com>
2021-06-08 11:35:30 +0200

    types: rename interface
    BREAKING CHANGE: change GetNgwLayerItemsOptions to GetNgwItemsOptions

--- packages/ngw-kit/src/utils/fetchNgwLayerFeatures.ts
+++ packages/ngw-kit/src/utils/fetchNgwLayerFeatures.ts
@@ -2,14 +2,14 @@ import { Geometry, Feature } from 'geojson';
 
 import { FeatureItem } from '@nextgis/ngw-connector';
 import CancelablePromise from '@nextgis/cancelable-promise';
-import { FetchNgwLayerItemsOptions } from '../interfaces';
+import { FetchNgwItemsOptions } from '../interfaces';
 import { fetchNgwLayerItems } from './fetchNgwLayerItems';
 import { createGeoJsonFeature } from './featureLayerUtils';
 
 export function fetchNgwLayerFeatures<
   G extends Geometry | null = Geometry,
   P extends { [field: string]: any } = { [field: string]: any }
->(options: FetchNgwLayerItemsOptions<P>): CancelablePromise<Feature<G, P>[]> {
+>(options: FetchNgwItemsOptions<P>): CancelablePromise<Feature<G, P>[]> {
   return fetchNgwLayerItems(options).then((x: FeatureItem[]) => {
     const features: Array<Feature<G, P>> = [];
     x.forEach((y) => {
