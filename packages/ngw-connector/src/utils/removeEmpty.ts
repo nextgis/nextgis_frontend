720eabe7645a66fc3addd118c724679af6264652
Artemiy <rendrom@gmail.com>
2021-07-12 21:09:54 +0200

    fix(utils): fix objectDeepEqual function

---
+++ packages/ngw-connector/src/utils/removeEmpty.ts
@@ -0,0 +1,8 @@
+export function removeEmpty<T extends Record<any, any>>(obj: T): T {
+  const newObj: Record<any, any> = {} as T;
+  Object.keys(obj).forEach((key) => {
+    if (obj[key] === Object(obj[key])) newObj[key] = removeEmpty(obj[key]);
+    else if (obj[key] !== undefined) newObj[key] = obj[key];
+  });
+  return newObj;
+}
