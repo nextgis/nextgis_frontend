64b10e844281a38585f3c9a870c751a44600c195
Artemiy <rendrom@gmail.com>
2021-07-16 14:04:36 +0200

    fix(ngw-connector): disable request params list convert to object

--- packages/ngw-connector/src/utils/removeEmpty.ts
+++ packages/ngw-connector/src/utils/removeEmpty.ts
@@ -1,8 +1,11 @@
 export function removeEmpty<T extends Record<any, any>>(obj: T): T {
   const newObj: Record<any, any> = {} as T;
   Object.keys(obj).forEach((key) => {
-    if (obj[key] === Object(obj[key])) newObj[key] = removeEmpty(obj[key]);
-    else if (obj[key] !== undefined) newObj[key] = obj[key];
+    if (!(obj[key] instanceof Array) && obj[key] === Object(obj[key])) {
+      newObj[key] = removeEmpty(obj[key]);
+    } else if (obj[key] !== undefined) {
+      newObj[key] = obj[key];
+    }
   });
   return newObj;
 }
