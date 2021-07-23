b40df4d8742e970060e3eee60c6549d53567b938
Artemiy <rendrom@gmail.com>
2021-07-23 21:29:52 +0200

    feat(ngw-kit): update loaded date before property filter

---
+++ packages/ngw-kit/src/utils/prepareNgwFieldsToPropertiesFilter.ts
@@ -0,0 +1,28 @@
+import { isObject, defined } from '@nextgis/utils';
+import type { NgwDateFormat, NgwDateTimeFormat } from '@nextgis/ngw-connector';
+
+export function prepareNgwFieldsToPropertiesFilter(
+  fields: Record<string, any>,
+): Record<string, any> {
+  let f: keyof typeof fields;
+  for (f in fields) {
+    const field = fields[f];
+    if (isObject(field)) {
+      const date = field as NgwDateFormat | NgwDateTimeFormat;
+      if (defined(date.year) && defined(date.month) && defined(date.day)) {
+        const dt: [number, number, number, number?, number?, number?] = [
+          date.year,
+          date.month - 1,
+          date.day,
+        ];
+        if ('hour' in date) {
+          [date.hour, date.minute, date.second].forEach((x) => {
+            dt.push(x);
+          });
+        }
+        fields[f] = new Date(...dt).toISOString();
+      }
+    }
+  }
+  return fields;
+}
