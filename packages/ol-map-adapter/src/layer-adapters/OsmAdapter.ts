378de0a7bd149689718f9a0d13a7136e371d6e16
Artemiy <rendrom@gmail.com>
2021-08-19 20:26:20 +0200

    feat(ol-map-adapter): add setLayerOpacity mapAdapter and each layer methods

--- packages/ol-map-adapter/src/layer-adapters/OsmAdapter.ts
+++ packages/ol-map-adapter/src/layer-adapters/OsmAdapter.ts
@@ -1,15 +1,19 @@
 import OSM from 'ol/source/OSM';
 import { ATTRIBUTION } from 'ol/source/OSM';
 import TileLayer from 'ol/layer/Tile';
+import { BaseAdapter } from './BaseAdapter';
 
 import type Map from 'ol/Map';
 import type { MainLayerAdapter, AdapterOptions } from '@nextgis/webmap';
-export class OsmAdapter implements MainLayerAdapter {
-  name = 'osm';
+export class OsmAdapter extends BaseAdapter implements MainLayerAdapter {
+  name = 'OpenStreetMap';
 
-  constructor(public map: Map, public options: AdapterOptions) {}
+  constructor(public map: Map, public options: AdapterOptions) {
+    super(map, options);
+  }
 
   addLayer(): TileLayer<OSM> {
+    this.options.name = this.name;
     const attributions = [ATTRIBUTION];
     const layer = new TileLayer({
       source: new OSM({
