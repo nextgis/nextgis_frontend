336a66428a81584d87e9491db0b2ead3e8d06fb7
Artemiy <rendrom@gmail.com>
2021-08-25 16:06:32 +0200

    chore: code cleanup

--- docs/typedoc.js
+++ docs/typedoc.js
@@ -1,15 +1,13 @@
 const findPackages = require('../scripts/findPackages');
 
-const filteredPackages = findPackages().filter((x) => {
+const allowPackages = findPackages().filter((x) => {
   return !x.name.includes('vue');
 });
-
-const entryPoints = filteredPackages.map((x) => x.path + '/src/index.ts');
+const entryPoints = allowPackages.map((x) => x.path + '/src/index.ts');
 
 module.exports = {
   out: 'build',
   entryPoints,
-  exclude: ['**/node_modules/**', '**/*.spec.ts', '../demo', '../tests'],
   name: 'NextGIS Frontend',
   excludePrivate: true,
 };
