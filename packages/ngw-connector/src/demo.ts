e7d9fdbcf0bc8d4b8edc1cbf9e66f3263332de42
Artemiy <rendrom@gmail.com>
2021-10-03 19:56:46 +0200

    docs: rewrite examples to es6

--- packages/ngw-connector/src/demo.ts
+++ packages/ngw-connector/src/demo.ts
@@ -1,40 +1,58 @@
-var ngwMap = new NgwMap({
+const ID = 'layer-id';
+const logBlock = document.getElementById('log');
+const log = function (msg) {
+  logBlock.innerHTML = '<p>' + msg + '</p>' + logBlock.innerHTML;
+  console.log(msg);
+};
+
+NgwMap.create({
   baseUrl: 'https://demo.nextgis.com',
   target: 'map',
-  bounds: [0, -90, 180, 90],
-  resources: [
-    // 1 basemap resource
-    1665,
-    // add a non-base layer in the middle, it will be higher than any base layer
-    {
-      resource: 1734,
-      fit: true,
-    },
-    // 2 basemap with opacity parameter
-    {
-      resource: 466,
-      opacity: 0.8,
-    },
-    // 3 basemap resource
-    4142,
-  ],
-  controls: ['ZOOM', 'ATTRIBUTION', 'switch_basemap'],
-  controlsOptions: {
-    switch_basemap: {
-      control: 'BUTTON',
-      html: '>>',
-      title: 'Switch basemap layer',
-      onClick: function () {
-        const activeBaseLayer = ngwMap.getActiveBaseLayer();
-        const baseLayers = ngwMap.getBaseLayersIds();
-        const index = baseLayers.indexOf(activeBaseLayer.id);
-        const nextBaseLayer = baseLayers[(index + 1) % baseLayers.length];
-        ngwMap.showLayer(nextBaseLayer);
+  osm: true,
+}).then((ngwMap) => {
+  ngwMap.addNgwLayer({
+    id: ID,
+    resource: 4222,
+    fit: true,
+    adapterOptions: {
+      limit: 100,
+      onClick: function (e) {
+        log('`click` - from options');
+      },
+      onMouseOver: function (e) {
+        log('`mouseover` - from options');
+      },
+      onMouseOut: function (e) {
+        log('`mouseout` - from options');
       },
     },
-  },
-});
+  });
 
-ngwMap.onLoad().then(function () {
-  //
+  ngwMap.emitter.on('layer-' + ID + ':click', function (ev) {
+    log('`click` -- from specific layer emitter');
+  });
+
+  ngwMap.emitter.on('layer-' + ID + ':mouseover', function (ev) {
+    log('`mouseover` -- from specific layer emitter');
+  });
+
+  ngwMap.emitter.on('layer-' + ID + ':mouseout', function (ev) {
+    log('`mouseout` -- from specific layer emitter');
+  });
+
+  ngwMap.emitter.on('layer:click', function (ev) {
+    if (ev.layer.id === ID) {
+      log('`click` --- from global emitter');
+    }
+  });
+  ngwMap.emitter.on('layer:mouseover', function (ev) {
+    if (ev.layer.id === ID) {
+      log('`mouseover` --- from global emitter');
+    }
+  });
+  ngwMap.emitter.on('layer:mouseout', function (ev) {
+    if (ev.layer.id === ID) {
+      log('`mouseout` --- from global emitter');
+    }
+  });
 });
