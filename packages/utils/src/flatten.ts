eebf648f4deaf4743c0afc859c2793c9d77a4922
Artemiy <rendrom@gmail.com>
2021-10-05 18:31:34 +0200

    feat(utils): add options to flatten

--- packages/utils/src/flatten.ts
+++ packages/utils/src/flatten.ts
@@ -1,9 +1,17 @@
-export function flatten(data: Record<string, any>): Record<string, any> {
+export interface FlattenOptions {
+  flatArray?: boolean;
+}
+
+export function flatten(
+  data: Record<string, any>,
+  opt: FlattenOptions = {},
+): Record<string, any> {
+  const flatArray = opt.flatArray ?? true;
   const result: Record<string, any> = {};
   function recurse(cur: Record<string, any>, prop: any) {
     if (Object(cur) !== cur) {
       result[prop] = cur;
-    } else if (Array.isArray(cur)) {
+    } else if (Array.isArray(cur) && flatArray) {
       const l = cur.length;
       for (let i = 0; i < l; i++) {
         recurse(cur[i], prop + '[' + i + ']');
