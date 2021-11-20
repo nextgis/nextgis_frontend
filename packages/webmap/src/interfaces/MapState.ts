f18c57ad2f012f66fd5df8ad00f5ce5e7d7aaa92
Artemiy <rendrom@gmail.com>
2021-11-20 16:38:47 +0200

    types: remove useless extend from any type

--- packages/webmap/src/interfaces/MapState.ts
+++ packages/webmap/src/interfaces/MapState.ts
@@ -1,6 +1,6 @@
 import type { WebMapEvents } from './Events';
 
-export interface MapStateItem<V extends any = any> {
+export interface MapStateItem<V = any> {
   event: keyof WebMapEvents;
   name: string;
   getValue: () => V;
