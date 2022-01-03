4d6d11dbd5b13389c174f3ee840146c1a3aaca59
Artemiy <rendrom@gmail.com>
2022-01-03 16:17:23 +0200

    fix(ngw-uploader): fix createGroup helper method

--- packages/ngw-uploader/examples/node-script/uploader.js
+++ packages/ngw-uploader/examples/node-script/uploader.js
@@ -1,18 +1,21 @@
 const NgwUploader = require('@nextgis/ngw-uploader');
+const NgwConnector = require('@nextgis/ngw-connector');
 // const NgwUploader = require('../../lib/ngw-uploader.cjs');
 
 const fs = require('fs');
 const path = require('path');
-
+const groupKeyname = 'ngw-uploader-node-example-group';
 const baseUrl = 'https://sandbox.nextgis.com';
 
-async function upload(filename, paint) {
+const connector = new NgwConnector({
+  baseUrl,
+  auth: { login: 'administrator', password: 'demodemo' },
+});
+const uploader = new NgwUploader({ connector });
+
+async function upload(filename, parentId, paint) {
   const file = fs.readFileSync(path.join(__dirname, filename));
 
-  const uploader = new NgwUploader({
-    baseUrl,
-    auth: { login: 'administrator', password: 'demodemo' },
-  });
   try {
     console.log(`start "${filename}" uploading`);
 
@@ -20,7 +23,7 @@ async function upload(filename, paint) {
       { file, name: filename },
       {
         addTimestampToName: true,
-        parentId: 0,
+        parentId,
         paint,
       },
     );
@@ -30,19 +33,37 @@ async function upload(filename, paint) {
   }
 }
 
-upload('buildings.shp.zip', {
-  fillColor: 'red',
-  strokeColor: '#C400AB',
-  opacity: 0.5,
-  weight: 2,
-});
-upload('railway.geojson', {
-  color: 'black',
-  opacity: 0.8,
-  weight: 3,
-});
-upload('railway_stations.shp.zip', {
-  color: 'blue',
-  strokeColor: 'white',
-  radius: 12,
+const uploadLayers = (parentId) => {
+  upload('buildings.shp.zip', parentId, {
+    fillColor: 'red',
+    strokeColor: '#C400AB',
+    opacity: 0.5,
+    weight: 2,
+  });
+  upload('railway.geojson', parentId, {
+    color: 'black',
+    opacity: 0.8,
+    weight: 3,
+  });
+  upload('railway_stations.shp.zip', parentId, {
+    color: 'blue',
+    strokeColor: 'white',
+    radius: 12,
+  });
+};
+
+connector.getResource(groupKeyname).then((existGroup) => {
+  if (existGroup) {
+    uploadLayers(existGroup.resource.id);
+  } else {
+    uploader
+      .createGroup({
+        parentId: 0,
+        displayName: groupKeyname,
+        keyname: groupKeyname,
+      })
+      .then((newGroup) => {
+        uploadLayers(newGroup.id);
+      });
+  }
 });
