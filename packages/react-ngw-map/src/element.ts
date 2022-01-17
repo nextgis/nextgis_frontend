3c9f99dbcfd34b76690bc8a5e8c62a5514fbd42a
Artemiy <rendrom@gmail.com>
2022-01-17 22:49:13 +0200

    wip: add react-ngw-map library

---
+++ packages/react-ngw-map/src/element.ts
@@ -0,0 +1,56 @@
+import { MutableRefObject, useEffect, useRef } from 'react';
+
+import { NgwMapContextInterface } from './context';
+
+export interface NgwMapElement<T, C = any> {
+  instance: T;
+  context: NgwMapContextInterface;
+  container?: C | null;
+}
+
+export type ElementHook<E, P> = (
+  props: P,
+  context: NgwMapContextInterface,
+) => MutableRefObject<NgwMapElement<E>>;
+
+export function createElementHook<E, P, C = any>(
+  createElement: (
+    props: P,
+    context: NgwMapContextInterface,
+  ) => NgwMapElement<E>,
+  updateElement?: (instance: E, props: P, prevProps: P) => void,
+) {
+  if (updateElement === null) {
+    return function useImmutableNgwMapElement(
+      props: P,
+      context: NgwMapContextInterface,
+    ): ReturnType<ElementHook<E, P>> {
+      return useRef<NgwMapElement<E, C>>(createElement(props, context));
+    };
+  }
+
+  return function useMutableNgwMapElement(
+    props: P,
+    context: NgwMapContextInterface,
+  ): ReturnType<ElementHook<E, P>> {
+    const elementRef = useRef<NgwMapElement<E, C>>(
+      createElement(props, context),
+    );
+    const propsRef = useRef<P>(props);
+    const { instance } = elementRef.current;
+
+    useEffect(
+      function updateElementProps() {
+        if (propsRef.current !== props) {
+          if (updateElement) {
+            updateElement(instance, props, propsRef.current);
+          }
+          propsRef.current = props;
+        }
+      },
+      [instance, props, context],
+    );
+
+    return elementRef;
+  };
+}
--- packages/vuex-ngw/README.md
+++ packages/vuex-ngw/README.md
@@ -4,6 +4,6 @@ Vuex store for NGW resources
 
 ## Commercial support
 
-Need to fix a bug or add a feature to @nextgis/vuex-ngw? We provide custom development and support for this software. [Contact us](http://nextgis.com/contact/) to discuss options!
+Need to fix a bug or add a feature to `@nextgis/vuex-ngw`? We provide custom development and support for this software. [Contact us](http://nextgis.com/contact/) to discuss options!
 
 [![http://nextgis.com](https://nextgis.ru/img/nextgis.png)](http://nextgis.com)
