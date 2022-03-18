7904dba7b094ee09672643aa23bd7ae0fc4f6428
Artemiy <rendrom@gmail.com>
2022-03-18 14:42:45 +0200

    feat(ngw-connector): add recursive option to getChildrenOf

--- packages/ngw-connector/src/types/ResourceStore.ts
+++ packages/ngw-connector/src/types/ResourceStore.ts
@@ -2,7 +2,7 @@ export type ResourceStoreItemProperties = { [name: string]: any } | null;
 
 export type ResourceStoreItem<
   P extends ResourceStoreItemProperties = ResourceStoreItemProperties,
-> = ResourceStoreItemDefault & {data?: string} & P;
+> = ResourceStoreItemDefault & { data?: string } & P;
 
 export interface ResourceStoreItemDefault {
   [field: string]: string | number | boolean | null | Date;
