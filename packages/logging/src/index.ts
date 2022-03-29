2f005fc2950ff152787ec8919fcf20229be27f6b
Artemiy <rendrom@gmail.com>
2022-03-29 15:35:10 +0200

    types(logging): console engine options

--- packages/logging/src/index.ts
+++ packages/logging/src/index.ts
@@ -4,7 +4,8 @@
 
 import { Logging } from './Logging';
 import { NgLogEngine } from './engines/NgLogEngine';
+import { ConsoleEngine } from './engines/ConsoleEngine';
 
 export * from './interfaces';
 
-export { Logging, NgLogEngine };
+export { Logging, ConsoleEngine, NgLogEngine };
