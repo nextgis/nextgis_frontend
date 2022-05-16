42ba2883f0800988f0a49844feedbbdb5b9f625b
Artemiy <rendrom@gmail.com>
2022-05-16 17:17:39 +0200

    chore: change index.js prod check for cjs

--- packages/area/index.js
+++ packages/area/index.js
@@ -1,8 +1,8 @@
 'use strict';
 
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./lib/area.cjs.prod.js');
-} else {
+if (process.env.NODE_ENV === 'development') {
   module.exports = require('./lib/area.cjs.js');
+} else {
+  module.exports = require('./lib/area.cjs.prod.js');
 }
 //# sourceMappingURL=./lib/area.esm-bundler.js.map
