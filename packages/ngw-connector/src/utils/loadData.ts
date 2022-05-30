aa3baa451989e47820adf07d67a771967688687b
Artemiy <rendrom@gmail.com>
2022-05-30 20:34:49 +0200

    feat(ngw-connector): disable transfer-encoding for node requests II

--- packages/ngw-connector/src/utils/loadData.ts
+++ packages/ngw-connector/src/utils/loadData.ts
@@ -2,8 +2,10 @@ import {
   RequestOptions as NgwRequestOptions,
   RequestMethods,
 } from '../interfaces';
-import { NgwError } from '../errors/NgwError';
 import { NetworkError } from '../errors/NetworkError';
+import { extractError } from '../errors/extractError';
+import { NgwError } from '../errors/NgwError';
+import { isError } from '../errors/isError';
 import { isObject } from './isObject';
 
 // readyState
@@ -211,11 +213,7 @@ if (__BROWSER__) {
           });
         }
 
-        Object.assign(requestOpt.headers, {
-          'transfer-encoding': '',
-        });
-
-        if (body !== undefined && method !== 'POST') {
+        if (body !== undefined) {
           Object.assign(requestOpt.headers, {
             'content-type': 'application/json',
             'content-length': Buffer.byteLength(body),
@@ -239,10 +237,13 @@ if (__BROWSER__) {
                   }
                 } catch (er) {
                   reject(er);
-                  // throw new Error(er);
                 }
                 if (json !== undefined) {
-                  resolve(json);
+                  if (isError(json)) {
+                    reject('extractError(json)');
+                  } else {
+                    resolve(json);
+                  }
                 }
               }
             }
