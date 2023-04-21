0dc2491ecc53b8025e532a6c9dc881ce0677b2bc
Artemiy <rendrom@gmail.com>
2023-04-21 16:11:23 +0200

    chore: remove vuex-ngw type check

--- scripts/testBuildCmd.js
+++ scripts/testBuildCmd.js
@@ -5,8 +5,8 @@ const notValid = Object.entries(checkBuild());
 if (notValid.length) {
   console.error(
     `${chalk.bgRed.white(' ERROR ')} ${chalk.red(
-      `invalid build for packages:`
-    )}\n\n` + chalk.red(`${notValid.map((x) => x.join(', ')).join('\n')}`)
+      `invalid build for packages:`,
+    )}\n\n` + chalk.red(`${notValid.map((x) => x.join(', ')).join('\n')}`),
   );
   process.exit(1);
 } else {
