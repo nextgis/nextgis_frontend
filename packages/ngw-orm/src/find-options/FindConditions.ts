d4e45f0b9b9842774c46e716a9d97941ebc467cb
Artemiy <rendrom@gmail.com>
2023-05-13 12:42:55 +0200

    types: fix property filter exports

--- packages/ngw-orm/src/find-options/FindConditions.ts
+++ packages/ngw-orm/src/find-options/FindConditions.ts
@@ -1,9 +1,9 @@
-import { PropertiesFilter } from '@nextgis/properties-filter';
+import { PropertiesFilter, Properties } from '@nextgis/properties-filter';
 
-export type FindConditions<Entity> =
-  | PropertiesFilter<Entity>
-  | ObjectEqualConditions<Entity>;
+export type FindConditions<T extends Properties = Properties> =
+  | PropertiesFilter<T>
+  | ObjectEqualConditions<T>;
 
-export type ObjectEqualConditions<Entity> = {
-  [P in keyof Entity]?: any;
+export type ObjectEqualConditions<T extends Properties = Properties> = {
+  [P in keyof T]?: any;
 };
