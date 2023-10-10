1e212915962a9c68b27bbbb1d656e6cb283d5aed
Artemiy <rendrom@gmail.com>
2023-10-10 10:31:02 +0200

    fix(react-ngw-map): do not recreate ngwMap on props change

--- packages/react-ngw-map/src/hooks/useMapElement.ts
+++ packages/react-ngw-map/src/hooks/useMapElement.ts
@@ -1,47 +1,45 @@
 import { useEffect, useState, useRef } from 'react';
 import { NgwMap } from '@nextgis/ngw-map';
 
-import { shallowEqual } from '../utils/shallowEqual';
-
 import type { MutableRefObject } from 'react';
 import type { MapContainerProps } from '../interfaces';
 
 export function useMapElement(
   mapRef: MutableRefObject<HTMLElement | null>,
-  { center, zoom, ...restProps }: MapContainerProps,
+  props: MapContainerProps,
 ): NgwMap | null {
+  const { center, zoom } = props;
+  const isReady = useRef(false);
   const [ngwMap, setNgwMap] = useState<NgwMap | null>(null);
 
-  const prevPropsRef = useRef<MapContainerProps | null>(null);
-
   useEffect(() => {
-    const propsChanged = prevPropsRef.current
-      ? !shallowEqual(prevPropsRef.current, restProps)
-      : true;
 
-    if (!mapRef.current || ngwMap || !propsChanged) return;
+    if (!mapRef.current || ngwMap) return;
 
     const newNgwMap = new NgwMap({
       target: mapRef.current,
-      ...restProps,
+      ...props,
     });
-
+    newNgwMap.onLoad().then(() => {
+      isReady.current = true;
+    })
     setNgwMap(newNgwMap);
 
-    if (propsChanged) {
-      prevPropsRef.current = restProps;
-    }
-
-    return () => {
-      newNgwMap.destroy();
-    };
-  }, [mapRef, restProps]);
+  }, [mapRef]);
 
   useEffect(() => {
-    if (ngwMap && center && zoom) {
+    if (ngwMap && isReady.current && center && zoom) {
       ngwMap.setView({ center, zoom });
     }
   }, [ngwMap, center, zoom]);
+
+  useEffect(() => {
+    return () => {
+      if (ngwMap) {
+        ngwMap.destroy();
+      }
+    }
+  }, [])
 
   return ngwMap;
 }
