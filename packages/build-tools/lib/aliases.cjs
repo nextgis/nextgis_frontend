03809049fbbcf4af58cc20e38b6a1edd5d89091a
Artemiy <rendrom@gmail.com>
2024-03-04 14:43:13 +0200

    test: update test environment

---
+++ packages/build-tools/lib/aliases.cjs
@@ -0,0 +1,35 @@
+const { lstatSync, readdirSync, readFileSync, existsSync } = require('fs');
+const { join, resolve } = require('path');
+
+const isDirectory = (source) => lstatSync(source).isDirectory();
+
+function generate(source = '', module = false) {
+  source = source || resolve(__dirname, '..', '..');
+  const items = {};
+  readdirSync(source).forEach((name) => {
+    const libPath = join(source, name);
+    // find packages
+    if (isDirectory(libPath)) {
+      const pkgPath = join(libPath, 'package.json');
+      if (existsSync(pkgPath)) {
+        const pkg = JSON.parse(readFileSync(pkgPath, 'utf8'));
+
+        if (module) {
+          if (pkg.module) {
+            const module = join(libPath, pkg.module);
+            if (existsSync(module)) {
+              items[pkg.name + '$'] = module;
+            }
+          }
+        } else {
+          items[pkg.name + '$'] = join(libPath, '/src/index.ts');
+        }
+      }
+    }
+  });
+  return items;
+}
+
+module.exports = (source, module) => {
+  return generate(source, module);
+};
