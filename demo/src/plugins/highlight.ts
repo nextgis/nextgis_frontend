d61f3a9684fa8340272b13c34264a59843f50b8b
Artemiy <rendrom@gmail.com>
2024-03-15 12:21:48 +0200

    docs: highliht demo examples css

--- demo/src/plugins/highlight.ts
+++ demo/src/plugins/highlight.ts
@@ -2,6 +2,7 @@ import 'highlight.js/styles/github.css';
 
 import hljs from 'highlight.js/lib/core';
 import bash from 'highlight.js/lib/languages/bash';
+import css from 'highlight.js/lib/languages/css';
 import javascript from 'highlight.js/lib/languages/javascript';
 import plaintext from 'highlight.js/lib/languages/plaintext';
 import xml from 'highlight.js/lib/languages/xml';
@@ -10,6 +11,7 @@ hljs.registerLanguage('javascript', javascript);
 hljs.registerLanguage('bash', bash);
 hljs.registerLanguage('html', xml);
 hljs.registerLanguage('xml', xml);
+hljs.registerLanguage('css', css);
 hljs.registerLanguage('plaintext', plaintext);
 
 export { hljs };
