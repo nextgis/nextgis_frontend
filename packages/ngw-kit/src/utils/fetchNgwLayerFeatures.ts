72179f21f082f6c9f55ff3d77d22342ec12ec42a
Artemiy <rendrom@gmail.com>
2024-07-09 13:26:18 +0200

    refactor(ngw-kit): rewrite to async

--- packages/ngw-kit/src/utils/fetchNgwLayerFeatures.ts
+++ packages/ngw-kit/src/utils/fetchNgwLayerFeatures.ts
@@ -2,20 +2,18 @@ import { createGeoJsonFeature } from './featureLayerUtils';
 import { fetchNgwLayerItems } from './fetchNgwLayerItems';
 
 import type { FetchNgwItemsOptions } from '../interfaces';
-import type { FeatureItem } from '@nextgis/ngw-connector';
+
 import type { FeatureProperties } from '@nextgis/utils';
 import type { Feature, Geometry } from 'geojson';
 
-export function fetchNgwLayerFeatures<
+export async function fetchNgwLayerFeatures<
   G extends Geometry | null = Geometry,
   P extends FeatureProperties = FeatureProperties,
 >(options: FetchNgwItemsOptions<P>): Promise<Feature<G, P>[]> {
-  return fetchNgwLayerItems(options).then((x: FeatureItem[]) => {
-    const features: Array<Feature<G, P>> = [];
-    x.forEach((y) => {
-      features.push(createGeoJsonFeature(y));
-    });
-
-    return features;
+  const x = await fetchNgwLayerItems(options);
+  const features: Array<Feature<G, P>> = [];
+  x.forEach((y) => {
+    features.push(createGeoJsonFeature(y));
   });
+  return features;
 }
