226c8941c7fddc9e245c02bf80e69dce04e24d94
Artemiy <rendrom@gmail.com>
2024-08-29 23:53:01 +0200

    feat(utils): add updateUrlParams util

--- packages/utils/src/url.ts
+++ packages/utils/src/url.ts
@@ -2,3 +2,27 @@ export function fixUrlStr(url: string): string {
   // remove double slash
   return url.replace(/([^:]\/)\/+/g, '$1');
 }
+
+export function updateUrlParams(
+  urlStr: string,
+  params: Record<string, string | undefined>,
+) {
+  const url = new URL(urlStr);
+  const searchParams = new URLSearchParams(url.search);
+
+  Object.entries(params).forEach(([key, value]) => {
+    if (value === undefined) {
+      searchParams.delete(key);
+    } else {
+      searchParams.set(key, value);
+    }
+  });
+
+  searchParams.set('timestamp', String(new Date().getTime()));
+
+  const newSearch = [...searchParams.entries()]
+    .map(([key, value]) => `${key}=${value}`)
+    .join('&');
+
+  return `${url.origin}${url.pathname}?${newSearch}`;
+}
